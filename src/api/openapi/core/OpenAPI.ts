236253f1298d04faa30d70357e0adc75bb2eb817
Cazcade <wolfcarves@gmail.com>
2023-11-07 21:01:28 +0800

    fix: openapi config typo again wtf

--- src/api/openapi/core/OpenAPI.ts
+++ src/api/openapi/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-  BASE: "https//:qc-agrihub.xyz",
+  BASE: "https://qc-agrihub.xyz",
   VERSION: "1.0.0",
   WITH_CREDENTIALS: true,
   CREDENTIALS: "include",

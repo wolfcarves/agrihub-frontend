0c757d25c3e04ca297fd4b5d25ba1557243721ba
Danielzxccc <araojodaniel321@gmail.com>
2023-12-11 04:03:37 +0800

    analytics initial

--- src/pages/user/community/tabs/analytics.tsx
+++ src/pages/user/community/tabs/analytics.tsx
@@ -1,7 +1,80 @@
 import React from "react";
 
+import { Bar, Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
 const Analytics = () => {
-  return <div>analytics</div>;
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  };
+
+  const farmsData = {
+    January: 37,
+    February: 12,
+    March: 85,
+    April: 64,
+    May: 22,
+    June: 91,
+    July: 50,
+    August: 76,
+    September: 18,
+    October: 3,
+    November: 95,
+    December: 42
+  };
+
+  const farms = {
+    labels: Object.keys(farmsData),
+    datasets: [
+      {
+        label: "Growth Rate",
+        data: Object.values(farmsData),
+        backgroundColor: ["#89c26f"],
+        borderColor: "#89c26f",
+        borderWidth: 2
+      }
+    ]
+  };
+
+  const cropsData = {
+    kamatis: 60,
+    talong: 20,
+    petchay: 30,
+    sili: 80,
+    carrots: 90,
+    kangkong: 140
+  };
+
+  const crops = {
+    labels: Object.keys(cropsData),
+    datasets: [
+      {
+        label: "Production Rate",
+        data: Object.values(cropsData),
+        backgroundColor: ["#89c26f"],
+        borderColor: "#89c26f",
+        borderWidth: 2
+      }
+    ]
+  };
+
+  return (
+    <div className="grid grid-cols-2 grid-rows-1">
+      <div className="h-[400px] border-black border-1 p-1">
+        <Bar data={farms} options={chartOptions} />
+      </div>
+      <div className="h-[400px] border-black border-1 p-1">
+        <Line data={crops} options={chartOptions} />
+      </div>
+    </div>
+  );
 };
 
 export default Analytics;

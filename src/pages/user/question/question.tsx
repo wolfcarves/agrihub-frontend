0eb20cec124b7922c4ac40a2e3e25b3e80009757
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-12-17 23:52:19 +0800

    chore: init question

--- src/pages/user/question/question.tsx
+++ src/pages/user/question/question.tsx
@@ -2,13 +2,15 @@
   path - /question/:username/:questionId/:questionTitle
 */
 
-import { TbUserQuestion } from "react-icons/tb";
 import { useNavigate, useParams } from "react-router-dom";
-import { Button } from "../../../components/ui/button";
-import { FcShare } from "react-icons/fc";
 import { useState } from "react";
 import useGetViewQuestion from "../../../hooks/api/get/useGetViewQuestion";
-
+import { timeAgo } from "../../../components/lib/utils";
+import { UserAuth } from "../../../providers/AuthProvider";
+import { PiShareFatLight } from "react-icons/pi";
+import { BiSolidDownArrow, BiSolidUpArrow } from "react-icons/bi";
+import { CiBookmarkPlus } from "react-icons/ci";
+import { GoReport } from "react-icons/go";
 const Question = () => {
   const { username } = useParams();
   const { questionId } = useParams();
@@ -20,37 +22,46 @@ const Question = () => {
   };
   const { data } = useGetViewQuestion(questionId || "", String(page));
   console.log(data);
+  // const user = useSelector(getUserState);
+  // console.log(user);
+
+  const { data: users } = UserAuth() ?? {};
+
+  console.log("User data:", users);
+
   return (
-    <div className="p-3 max-h-full overflow-y-auto">
-      <div className="flex justify-end items-center mb-3">
-        <Button onClick={() => handleNavigateAsk()}>
-          <TbUserQuestion size={18} />
-          &nbsp;Ask Questions
-        </Button>
-      </div>
-      <div>
-        <h4 className="text-[1.4rem] font-normal text-primary leading-tight">
-          {data?.title}
-        </h4>
-        <div className="flex justify-start gap-4 text-[#636363] text-sm">
-          <div>
-            Ask <span className="text-black">Today</span>
-          </div>
-          <div>
-            Modified <span className="text-black">today</span>
-          </div>
-          <div>
-            Viewed <span className="text-black">19k</span>
-          </div>
-        </div>
-        <div className="flex gap-2 justify-end mt-2 text-[#636363] ">
-          <div className="flex items-center gap-1">Share</div>
-          <div>Edit</div>
-          <div>Follow</div>
+    <div className="p-3 max-h-full overflow-y-auto flex flex-col gap-4">
+      <div className="text-2xl font-bold leading-tight">{data?.title}</div>
+      <div className="col-span-3 flex gap-2 items-center flex-nowrap">
+        <img
+          src={data?.user.avatar}
+          className="w-11 h-11 object-center object-cover bg-slate-500 rounded-xl"
+        />
+        <div>
+          <h6 className=" font-semibold ">{data?.user?.username}</h6>
+          <p className="text-gray-400 text-sm">
+            {timeAgo(data?.createdat || "")}
+          </p>
         </div>
       </div>
-      <div>{data?.question}</div>
-      <div>{data?.tags.map((tags, i) => <div key={i}>{tags.tag}</div>)}</div>
+
+      <div
+        className="leading-loose"
+        dangerouslySetInnerHTML={{
+          __html: data?.question || "<p></p>"
+        }}
+      />
+      <div></div>
+      <div className="flex gap-2 justify-start">
+        {data?.tags.map((tags, i) => (
+          <span
+            key={i}
+            className="text-primary text-sm bg-secondary border border-primary py-1 px-2 rounded-lg"
+          >
+            {tags.tag}
+          </span>
+        ))}
+      </div>
       {data?.answer_count === "0" ? <div>No Comment</div> : <div>asdasdas</div>}
     </div>
   );

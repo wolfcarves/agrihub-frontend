0f9f6dc021b84e83fdc2675da64505c6e05783ed
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-12-22 00:38:37 +0800

    feat: topbar signin & signup

--- src/components/ui/custom/Topbar/Topbar.tsx
+++ src/components/ui/custom/Topbar/Topbar.tsx
@@ -2,15 +2,20 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import Logo from "@icons/main-logo.svg";
 import AgriLogo from "@icons/logoAgrihub.svg";
 import { FaRegBell } from "react-icons/fa6";
-import dp from "@assets/images/dp.svg";
 import { SheetTrigger } from "@components/ui/sheet";
 import SearchBar from "../search-bar/SearchBar";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { UserAuth } from "@providers/AuthProvider";
+import { Button } from "../../button";
 export default function Topbar() {
-  // const toggle = useSelector((state: RootState) => state.sidebar.toggle);
-  // const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { data: currentUser } = UserAuth() ?? {};
+  const handleSignup = () => {
+    navigate("/account/signup");
+  };
+  const handleLogin = () => {
+    navigate("/account/login");
+  };
 
   return (
     <div className="px-6 py-2 bg-white border-b border-border flex justify-between w-full  z-30">
@@ -26,7 +31,7 @@ export default function Topbar() {
         />
         <SearchBar />
       </div>
-      <div className="xl:flex hidden items-center justify-center gap-12 text-md   text-[#404040] font-medium">
+      <div className="xl:flex hidden items-center justify-center gap-12 text-md text-[#404040] font-medium">
         <NavLink to={"/"}>Home</NavLink>
         <NavLink
           to={"/forums"}
@@ -48,13 +53,22 @@ export default function Topbar() {
         <NavLink to={"/blog"}>Blogs</NavLink>
         <NavLink to={""}>About Us</NavLink>
       </div>
-      <div className="flex items-center gap-4">
-        <FaRegBell size={20} color={"#404040"} />
-        <img
-          className="h-[2rem] w-[2rem] aspect-square rounded-full shadow"
-          src={currentUser?.avatar}
-        />
-      </div>
+      {currentUser ? (
+        <div className="flex items-center gap-4">
+          <FaRegBell size={20} color={"#404040"} />
+          <img
+            className="h-[2rem] w-[2rem] aspect-square rounded-full shadow"
+            src={currentUser?.avatar}
+          />
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <Button variant={"outline"} onClick={handleLogin}>
+            Login
+          </Button>
+          <Button onClick={handleSignup}>Signup</Button>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/user/users/_layout.tsx
+++ src/pages/user/users/_layout.tsx
@@ -8,27 +8,21 @@ import { UsePagination } from "@providers/PaginationProvider";
 const UserProfileLayout = () => {
   const pagination = UsePagination();
   return (
-    <div className="flex flex-col max-h-screen h-screen">
-      <Sheet key={"sidebar"}>
-        <Topbar />
-        <Sidebar />
-        <div className="grid grid-cols-12 grid-rows-1 h-full overflow-y-auto">
-          <div className="col-span-2 lg:flex hidden my-6 pt-2 flex-col items-end pr-3 border-r border-border">
-            <SidebarLeft />
-          </div>
+    <div className="grid grid-cols-12 grid-rows-1 h-full overflow-y-auto">
+      <div className="col-span-2 lg:flex hidden my-6 pt-2 flex-col items-end pr-3 border-r border-border">
+        <SidebarLeft />
+      </div>
 
-          <div
-            className="lg:col-span-8 col-span-12 overflow-y-auto py-8 px-2 lg:mx-[5rem] mx-2 scroll-smooth"
-            ref={pagination?.topRef}
-          >
-            <Outlet />
-          </div>
+      <div
+        className="lg:col-span-8 col-span-12 overflow-y-auto py-8 px-2 lg:mx-[5rem] mx-2 scroll-smooth"
+        ref={pagination?.topRef}
+      >
+        <Outlet />
+      </div>
 
-          <div className="col-span-2 lg:flex flex-col hidden border-l border-border pl-3 pt-8">
-            <SidebarRight />
-          </div>
-        </div>
-      </Sheet>
+      <div className="col-span-2 lg:flex flex-col hidden border-l border-border pl-3 pt-8">
+        <SidebarRight />
+      </div>
     </div>
   );
 };

a01b2632b76e3d2712bd211c65d70ce46b83d597
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2023-12-26 02:58:06 -0800

    cards ni kent (#34)

--- src/pages/user/question/question-tags.tsx
+++ src/pages/user/question/question-tags.tsx
@@ -1,9 +1,14 @@
-/*
-  path - /questions/tags 
-*/
+import QuestionSearchBar from '@components/user/questions/qustion-search-bar/QuestionSearchBar';
+import TagsCard from '@components/user/questions/tags-card/TagsCard';
+
 
 const QuestionTags = () => {
-  return <div>Questions Tags Page</div>;
+  return (
+    <div>
+    <QuestionSearchBar/>
+    <TagsCard/>
+    </div>
+  )
 };
 
 export default QuestionTags;

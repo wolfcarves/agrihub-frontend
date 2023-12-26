a308b672613d6c6892a5ec5b9032411aaa48b851
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-12-26 16:35:51 +0800

    fix: id

--- src/components/user/questions/question-view/AnswerForm.tsx
+++ src/components/user/questions/question-view/AnswerForm.tsx
@@ -8,7 +8,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { AnswersSchema } from "../../../../api/openapi";
 interface AnswerFormProps {
-  questionId: string;
+  questionId: string | undefined;
 }
 const AnswerForm: React.FC<AnswerFormProps> = ({ questionId }) => {
   const { data: currentUser } = UserAuth() ?? {};
@@ -26,7 +26,7 @@ const AnswerForm: React.FC<AnswerFormProps> = ({ questionId }) => {
 
     try {
       await questionAnswerMutate({
-        questionId: questionId,
+        questionId: questionId || "",
         userAnswer: raw
       });
 

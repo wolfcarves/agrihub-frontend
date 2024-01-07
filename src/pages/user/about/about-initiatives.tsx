217f4aa702036966ca680ca87f3008d73f8ec445
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2024-01-07 17:53:44 +0800

    Landing Page v1

--- src/pages/user/about/about-initiatives.tsx
+++ src/pages/user/about/about-initiatives.tsx
@@ -1,7 +1,16 @@
+import SeeMore from "@components/user/landing/Button/SeeMore";
+import ContentInitiatives from "@components/user/landing/initiatives/ContentInitiatives";
+import Imagebanner from "@components/user/landing/initiatives/imagebanner";
 import React from "react";
 
 const AboutInitiatives = () => {
-  return <div>about-initiatives</div>;
+  return (
+    <div>
+      <Imagebanner/>
+      <ContentInitiatives/>
+      <SeeMore/>
+    </div>
+  );
 };
 
 export default AboutInitiatives;

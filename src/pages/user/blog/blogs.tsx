89d2fbbfff5e740c9f9e129f108a29194aed1dc8
vince-dev023 <jan.vincentneal04@gmail.com>
2024-01-14 18:43:45 +0800

    recent blogs

--- src/pages/user/blog/blogs.tsx
+++ src/pages/user/blog/blogs.tsx
@@ -2,8 +2,98 @@ import React from "react";
 
 const Blogs = () => {
   return (
-    <div>
-      <p></p>
+    <div className="py-4 px-10 grid grid-cols-2 gap-0">
+      <div className="">
+        <h3>Recent Blogs</h3>
+
+        <img src="https://via.placeholder.com/750x480" className="pt-2"></img>
+
+        <h5 className="text-gray-600 pt-4 text-sm">October 19, 2023</h5>
+
+        <a className="font-bold text-3xl" href="#">
+          Permaculture Workshop Day 1
+        </a>
+
+        <p className="text-gray-600 text-sm me-8 text-justify">
+          Sa pangalawang araw ng workshop, itinalakay at pinag-usapan ang mas
+          malalim na kahulugan ng permakultura, ang buhay sa ilalim ng lupa,
+          bahagi, ethics, abstract principle, ekolohiya, stratehiya at mga
+          teknik sa permakultura. Sa tulong ni Sir Sixto Bereber, nagkaroon ng
+          mas malalim na kaalaman sa agrikultura at kalikasan ang ating mga
+          urban farmers na maaari nilang i-apply sa kani-kanilang mga urban
+          farms dito sa Quezon City. Ang pagsasanay na ito ay dahil sa
+          kolaborasyon ng Quezon City University - Center for Urban...
+        </p>
+      </div>
+
+      <div className="grid row-auto pt-11 me-3">
+        <div className="grid grid-cols-2">
+          <div>
+            <img src="https://via.placeholder.com/700x400"></img>
+          </div>
+          <div className="ms-3">
+            <h5 className="text-gray-500 text-sm">October 19, 2023</h5>
+            <a className="font-bold text-xl" href="#">
+              Permaculture Workshop Day 1
+            </a>
+            <p className="text-gray-600 text-xs text-justify">
+              Sa pangalawang araw ng workshop, itinalakay at pinag-usapan ang
+              mas malalim na kahulugan ng permakultura, ang buhay sa ilalim ng
+              lupa, bahagi, ethics, abstract principle, ekolohiya, stratehiya at
+              mga teknik sa permakultura. Sa tulong ni Sir Sixto Bereber,
+              nagkaroon ng mas malalim na kaalaman sa agrikultura at kalikasan
+              ang ating mga urban farmers na maaari nilang i-apply sa
+              kani-kanilang mga urban farms dito sa Quezon City. Ang pagsasanay
+              na ito ay dahil sa kolaborasyon ng Quezon City University - Center
+              for Urban...
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 pt-3">
+          <div>
+            <img src="https://via.placeholder.com/700x400"></img>
+          </div>
+          <div className="ms-3">
+            <h5 className="text-gray-500 text-sm">October 19, 2023</h5>
+            <a className="font-bold text-xl" href="#">
+              Permaculture Workshop Day 1
+            </a>
+            <p className="text-gray-600 text-xs text-justify">
+              Sa pangalawang araw ng workshop, itinalakay at pinag-usapan ang
+              mas malalim na kahulugan ng permakultura, ang buhay sa ilalim ng
+              lupa, bahagi, ethics, abstract principle, ekolohiya, stratehiya at
+              mga teknik sa permakultura. Sa tulong ni Sir Sixto Bereber,
+              nagkaroon ng mas malalim na kaalaman sa agrikultura at kalikasan
+              ang ating mga urban farmers na maaari nilang i-apply sa
+              kani-kanilang mga urban farms dito sa Quezon City. Ang pagsasanay
+              na ito ay dahil sa kolaborasyon ng Quezon City University - Center
+              for Urban...
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 pt-3">
+          <div>
+            <img src="https://via.placeholder.com/700x400"></img>
+          </div>
+          <div className="ms-3">
+            <h5 className="text-gray-500 text-sm">October 19, 2023</h5>
+            <a className="font-bold text-xl" href="#">
+              Permaculture Workshop Day 1
+            </a>
+            <p className="text-gray-600 text-xs text-justify">
+              Sa pangalawang araw ng workshop, itinalakay at pinag-usapan ang
+              mas malalim na kahulugan ng permakultura, ang buhay sa ilalim ng
+              lupa, bahagi, ethics, abstract principle, ekolohiya, stratehiya at
+              mga teknik sa permakultura. Sa tulong ni Sir Sixto Bereber,
+              nagkaroon ng mas malalim na kaalaman sa agrikultura at kalikasan
+              ang ating mga urban farmers na maaari nilang i-apply sa
+              kani-kanilang mga urban farms dito sa Quezon City. Ang pagsasanay
+              na ito ay dahil sa kolaborasynon ng Quezon City University -
+              Center for Urban...
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

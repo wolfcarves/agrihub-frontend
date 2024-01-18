cc6f94564cfaef9fa48b0080b925d85502748e34
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2024-01-18 18:12:49 +0800

    fix overlapping footer

--- src/pages/user/about/_layout.tsx
+++ src/pages/user/about/_layout.tsx
@@ -6,7 +6,6 @@ const AboutLayout = () => {
     <div className="grid grid-cols-12 grid-rows-1 h-full overflow-y-auto">
       <div className="col-span-12 overflow-y-auto pt-3 scroll-smooth">
         <Outlet />
-        <Footer />
       </div>
     </div>
   );

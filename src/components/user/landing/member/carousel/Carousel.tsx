3e2b884d16a052b61f0dce05994a959b74b655ba
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2024-01-24 00:50:29 -0800

    Kent landing final (#81)
    * FIX/images and Hor. scrollbr
    
    ---------
    
    Co-authored-by: Cazcade <rodel.crisosto7@gmail.com>
    Co-authored-by: nallalisa <117307206+nallalisa@users.noreply.github.com>

--- src/components/user/landing/member/carousel/Carousel.tsx
+++ src/components/user/landing/member/carousel/Carousel.tsx
@@ -1,9 +1,5 @@
 // Carousel.tsx
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
+import React, { useState } from "react";
 import imageData1 from '@assets/images/cover-photo-for-landing-page.png';
 import imageData2 from '@assets/images/OUR-FoCuS-COVER-PHOTO.png';
 import imageData3 from '@assets/images/Initiatives-cover-photo.png';
@@ -12,40 +8,58 @@ import imageData4 from '@assets/images/Latest-News-Cover-photo.png';
 const imageDatas = [imageData1, imageData2, imageData3, imageData4];
 
 const Carousel: React.FC = () => {
-  const sliderRef = React.useRef<Slider | null>(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % imageDatas.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? imageDatas.length - 1 : prevSlide - 1
+    );
   };
 
   return (
-    <div className="w-full h-1/3 relative">
-      <Slider ref={(slider) => (sliderRef.current = slider)} {...settings}>
-        {imageDatas.map((imageData, index) => (
-          <div key={index} className="relative h-full w-full">
+    <div className="m-0 overflow-x-hidden">
+      <div className="relative">
+        <div className="absolute top-1/2 left-0 transform -translate-y-1/2">
+          <button className="text-white px-4 py-2 mr-2" onClick={prevSlide}>
             <img
-              src={imageData}
-              alt={`Slide ${index + 1}`}
-              className="object-cover h-2/5 w-full max-w-full"
+              className="w-1/2 md:w-full lg:w-full xl:w-full hover:filter hover:brightness-75 transition duration-900 ease-in-out"
+              src="https://i.imgur.com/MRoecMt.png"
+              alt="Previous Image"
             />
-            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-              {[...Array(imageDatas.length)].map((_, dotIndex) => (
-                <div
-                  key={dotIndex}
-                  className={`w-6 h-2 bg-white rounded-full ${
-                    index === dotIndex ? 'opacity-100' : 'opacity-50'
-                  }`}
-                />
-              ))}
-            </div>
-          </div>
+          </button>
+        </div>
+        <div className="absolute top-1/2 right-0 transform -translate-y-1/2">
+          <button className="text-white px-4 py-2" onClick={nextSlide}>
+            <img
+              className="w-1/2 md:w-full lg:w-full xl:w-full hover:filter hover:brightness-75 transition duration-900 ease-in-out"
+              src="https://imgur.com/XhhddcW.png"
+              alt="Next Image"
+            />
+          </button>
+        </div>
+        <img
+          src={imageDatas[currentSlide]}
+          alt={`Slide ${currentSlide + 1}`}
+          className="w-full mt-3"
+          style={{ height: 'auto', minHeight: '200px', maxHeight: '100vh' }}
+        />
+          <div className="absolute bottom-0 left-0 right-0 flex justify-center mb-4">
+        {imageDatas.map((_, index) => (
+          <div
+            key={index}
+            className={`w-8 m:w-12 lg:w-24 px-5 h-1 bg-green mx-1 ${
+              index === currentSlide
+                ? 'bg-green-300'
+                : 'bg-gray-300' 
+            }`}
+          ></div>
         ))}
-      </Slider>
+      </div>
+      </div>   
     </div>
   );
 };

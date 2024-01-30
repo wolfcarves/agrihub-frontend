8fcde9fef2b57b7b65adcec8ffec958c2f67671b
anotherinthefire <rongodfreyultra@gmail.com>
2024-01-30 23:57:21 +0800

    feat: farm base pages

---
+++ src/pages/admin/farms/farms-admin.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FarmsAdmin = () => {
+  return <div></div>;
+};
+
+export default FarmsAdmin;
---
+++ src/pages/admin/farms/farms-rejected.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FarmsRejected = () => {
+  return <div></div>;
+};
+
+export default FarmsRejected;

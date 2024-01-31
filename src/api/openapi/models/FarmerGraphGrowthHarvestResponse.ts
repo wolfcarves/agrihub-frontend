03b5bda785efbeb8c4b12f09c14e59abeff44fdd
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-01-31 20:52:18 +0800

    chore: new service

---
+++ src/api/openapi/models/FarmerGraphGrowthHarvestResponse.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type FarmerGraphGrowthHarvestResponse = Array<{
+crop_name?: string;
+avg_harvest_qty?: string;
+avg_growth_span?: string;
+}>;

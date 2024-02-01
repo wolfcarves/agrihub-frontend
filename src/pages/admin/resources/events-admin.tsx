3c3c65d83f020e6eb9d58de42d220b51760d07c2
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-01 22:42:13 +0800

    feat: admin events management module

--- src/pages/admin/resources/events-admin.tsx
+++ src/pages/admin/resources/events-admin.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import AdminOutletContainer from "@components/admin/layout/container/AdminOutletContainer";
+import BreadCrumb from "../../../components/ui/custom/breadcrumb/breadcrumb";
+import { TableEvent } from "./table/table-event";
+import withAuthGuard from "@higher-order/account/withAuthGuard";
 
+const breadcrumbItems = [
+  { title: "Resource Management", link: "/admin/resources" },
+  { title: "Events", link: "/admin/resource/events" }
+];
 const EventsAdmin = () => {
-  return <div></div>;
+  return (
+    <AdminOutletContainer className="container mx-auto py-10 ">
+      <BreadCrumb items={breadcrumbItems} />
+      <h2 className="text-3xl font-bold tracking-tight">Events</h2>
+      <p className="text-sm text-muted-foreground">Manage all events.</p>
+      <hr className="my-4" />
+      <TableEvent />
+    </AdminOutletContainer>
+  );
 };
 
-export default EventsAdmin;
+export default withAuthGuard(EventsAdmin, ["admin", "asst_admin"]);
--- src/pages/admin/resources/events-draft.tsx
+++ src/pages/admin/resources/events-draft.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import AdminOutletContainer from "@components/admin/layout/container/AdminOutletContainer";
+import BreadCrumb from "../../../components/ui/custom/breadcrumb/breadcrumb";
+import { TableEvent } from "./table/table-event";
+import withAuthGuard from "@higher-order/account/withAuthGuard";
 
+const breadcrumbItems = [
+  { title: "Resource Management", link: "/admin/resources" },
+  { title: "Events", link: "/admin/resource/events" },
+  { title: "Drafs", link: "/admin/resource/events-draft" }
+];
 const EventsDraft = () => {
-  return <div></div>;
+  return (
+    <AdminOutletContainer className="container mx-auto py-10 ">
+      <BreadCrumb items={breadcrumbItems} />
+      <h2 className="text-3xl font-bold tracking-tight">Drafted Events</h2>
+      <p className="text-sm text-muted-foreground">Manage drafted events.</p>
+      <hr className="my-4" />
+      <TableEvent />
+    </AdminOutletContainer>
+  );
 };
 
-export default EventsDraft;
+export default withAuthGuard(EventsDraft, ["admin", "asst_admin"]);

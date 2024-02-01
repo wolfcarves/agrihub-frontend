d351f5fb0a96eced6d22d5d66819249fde4dfa17
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-01 22:41:15 +0800

    feat: admin learning management module

--- src/pages/admin/resources/learning-archive.tsx
+++ src/pages/admin/resources/learning-archive.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import AdminOutletContainer from "@components/admin/layout/container/AdminOutletContainer";
+import BreadCrumb from "../../../components/ui/custom/breadcrumb/breadcrumb";
+import { TableLearningMaterial } from "./table/table-learning";
+import withAuthGuard from "@higher-order/account/withAuthGuard";
 
-const LearningArchive = () => {
-  return <div></div>;
+const breadcrumbItems = [
+  { title: "Resource Management", link: "/admin/resources" },
+  { title: "Learning Materials", link: "/admin/resource/learnings" },
+  {
+    title: "Archived",
+    link: "/admin/resource/learnings-archives"
+  }
+];
+const LearningsArchive = () => {
+  return (
+    <AdminOutletContainer className="container mx-auto py-10 ">
+      <BreadCrumb items={breadcrumbItems} />
+      <h2 className="text-3xl font-bold tracking-tight">
+        Archived Learning Materials
+      </h2>
+      <p className="text-sm text-muted-foreground">
+        Manage archived learning materials.
+      </p>
+      <hr className="my-4" />
+      <TableLearningMaterial />
+    </AdminOutletContainer>
+  );
 };
 
-export default LearningArchive;
+export default withAuthGuard(LearningsArchive, ["admin", "asst_admin"]);

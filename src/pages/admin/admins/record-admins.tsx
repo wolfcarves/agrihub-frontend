f296e0d0a9d19e6ab5f245176c1177f4cfb288b4
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-01 23:07:26 +0800

    feat: admin admin management module

--- src/pages/admin/admins/record-admins.tsx
+++ src/pages/admin/admins/record-admins.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import AdminOutletContainer from "@components/admin/layout/container/AdminOutletContainer";
+import BreadCrumb from "../../../components/ui/custom/breadcrumb/breadcrumb";
+import { TableAdmin } from "./table/table-admin";
+import withAuthGuard from "@higher-order/account/withAuthGuard";
 
+const breadcrumbItems = [
+  { title: "Admin Management", link: "/admin/record/admins" },
+  { title: "Admin", link: "/admin/record/admins" }
+];
 const RecordAdmins = () => {
-  return <div></div>;
+  return (
+    <AdminOutletContainer className="container mx-auto py-10 ">
+      <BreadCrumb items={breadcrumbItems} />
+      <h2 className="text-3xl font-bold tracking-tight">Admin Accounts</h2>
+      <p className="text-sm text-muted-foreground">Manage all admins.</p>
+      <hr className="my-4" />
+      <TableAdmin />
+    </AdminOutletContainer>
+  );
 };
 
-export default RecordAdmins;
+export default withAuthGuard(RecordAdmins, ["admin", "asst_admin"]);

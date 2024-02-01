eb8527272efbc248ce015d1837f4f558d53f796f
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-01 23:02:21 +0800

    feat: admin farm management module

--- src/pages/admin/farms/farmers-banned.tsx
+++ src/pages/admin/farms/farmers-banned.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import AdminOutletContainer from "@components/admin/layout/container/AdminOutletContainer";
+import BreadCrumb from "../../../components/ui/custom/breadcrumb/breadcrumb";
+import { TableFarmers } from "./table/table-farmer";
+import withAuthGuard from "@higher-order/account/withAuthGuard";
 
+const breadcrumbItems = [
+  { title: "Farm Management", link: "/admin/farm" },
+  { title: "Farmers", link: "/admin/farm/farmers" },
+  { title: "Banned", link: "/admin/farm/accounts-banned" }
+];
 const FarmersBanned = () => {
-  return <div></div>;
+  return (
+    <AdminOutletContainer className="container mx-auto py-10 ">
+      <BreadCrumb items={breadcrumbItems} />
+      <h2 className="text-3xl font-bold tracking-tight">Banned Farmers</h2>
+      <p className="text-sm text-muted-foreground">
+        Manage banned farmers within the community.
+      </p>
+      <hr className="my-4" />
+      <TableFarmers />
+    </AdminOutletContainer>
+  );
 };
 
-export default FarmersBanned;
+export default withAuthGuard(FarmersBanned, ["admin", "asst_admin"]);
--- src/pages/admin/farms/farms.tsx
+++ src/pages/admin/farms/farms.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import BreadCrumb from "@components/ui/custom/breadcrumb/breadcrumb";
+import AdminOutletContainer from "@components/admin/layout/container/AdminOutletContainer";
+import BarDistrictOverview from "../charts/bar-district-overview";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription
+} from "@components/ui/card";
+import withAuthGuard from "@higher-order/account/withAuthGuard";
 
+const breadcrumbItems = [{ title: "Farm Management", link: "/admin/farm" }];
 const Farms = () => {
-  return <div></div>;
+  return (
+    <AdminOutletContainer className="container mx-auto py-10 ">
+      <BreadCrumb items={breadcrumbItems} />
+      <h2 className="text-3xl font-bold tracking-tight mb-5">Farm Overview</h2>
+
+      <div className="grid grid-cols-6 grid-rows-5 gap-4">
+        <Card className="col-span-2 flex justify-start items-center">
+          <CardHeader>
+            <CardTitle>10</CardTitle>
+            <CardDescription>Pending Farm Applications</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="col-span-2 col-start-3 flex justify-start items-center">
+          <CardHeader>
+            <CardTitle>127</CardTitle>
+            <CardDescription>Total Community Farm Registered</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="col-span-2 col-start-5 flex justify-start items-center">
+          <CardHeader>
+            <CardTitle>1840</CardTitle>
+            <CardDescription>Total Registered Farmer</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="col-span-6 row-span-4 row-start-2 p-5">
+          <BarDistrictOverview />
+        </Card>
+      </div>
+    </AdminOutletContainer>
+  );
 };
 
-export default Farms;
+export default withAuthGuard(Farms, ["admin", "asst_admin"]);

fe6fe357d056fc8729d52cd6c535ec62097b7dcc
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-02-05 18:06:24 +0800

    feat: edit farm profile

--- src/icons/Loader.tsx
+++ src/icons/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = ({ isVisible }: { isVisible?: boolean }) => {
   if (isVisible)
     return (
-      <div className="fixed inset-0 flex items-center justify-center bg-white/20 z-50">
+      <div className="fixed inset-0 flex items-center justify-center bg-white/70 z-50 select-none">
         <img src={"/infinite-spinner.svg"} width={80} height={80} />
       </div>
     );

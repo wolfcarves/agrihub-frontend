72098999424701e1898867c64afe598e3bd034b6
Danielzxccc <araojodaniel321@gmail.com>
2024-02-06 01:10:04 +0800

    chore: accept and reject invite

--- src/pages/user/invitation/invite-farm.tsx
+++ src/pages/user/invitation/invite-farm.tsx
@@ -1,14 +1,40 @@
 import React from "react";
 import { Button } from "@components/ui/button";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useGetFarmerInvitation from "@hooks/api/get/useGetFarmerInvitation";
-
+import useFarmAcceptInvitationMutation from "../../../hooks/api/post/useFarmAcceptInvitationMutation";
+import { toast } from "sonner";
+import useFarmRejectInvitationMutation from "../../../hooks/api/post/useFarmRejectInvitationMutation";
 const InviteFarm = () => {
   const { id } = useParams();
+  const { mutateAsync: acceptMutate, isLoading: acceptLoading } =
+    useFarmAcceptInvitationMutation();
+  const { mutateAsync: rejectMutate, isLoading: rejectLoading } =
+    useFarmRejectInvitationMutation();
+  const navigate = useNavigate();
 
   const { data, isError, isLoading } = useGetFarmerInvitation({
     id: id ? id : ""
   });
+
+  const handleAccept = async () => {
+    try {
+      await acceptMutate(id ? id : "");
+
+      navigate(`/community/my-community/${data?.id}`);
+    } catch (error) {
+      toast.error("Invitation Error");
+    }
+  };
+
+  const handleReject = async () => {
+    try {
+      await rejectMutate(id ? id : "");
+      navigate("/");
+    } catch (error) {
+      toast.error("Invitation Error");
+    }
+  };
 
   if (isLoading) return "Loading ...";
   return (
@@ -28,9 +54,22 @@ const InviteFarm = () => {
             You're invited to become a part of the community by
             <span className="text-green-800 font-bold"> {data.farm_name}</span>
           </div>
+          <div>{acceptLoading && "Loading..."}</div>
+          <div>{rejectLoading && "Loading..."}</div>
           <div className="mt-4 flex gap-2">
-            <Button>ACCEPT</Button>
-            <Button variant="outline">Decline</Button>
+            <Button
+              onClick={handleAccept}
+              disabled={rejectLoading || acceptLoading}
+            >
+              ACCEPT
+            </Button>
+            <Button
+              variant="outline"
+              onClick={handleReject}
+              disabled={rejectLoading || acceptLoading}
+            >
+              Decline
+            </Button>
           </div>
         </>
       )}

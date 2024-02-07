6ed154a8b8d15d10f1e489cbd3e04e1e3ad3c2fe
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-07 13:07:59 +0800

    feat: learning materials crud

--- src/pages/admin/resources/learnings/learnings-admin.tsx
+++ src/pages/admin/resources/learnings/learnings-admin.tsx
@@ -3,6 +3,19 @@ import AdminOutletContainer from "@components/admin/layout/container/AdminOutlet
 import BreadCrumb from "../../../../components/ui/custom/breadcrumb/breadcrumb";
 import { TableLearningMaterial } from "../table/table-learning";
 import withAuthGuard from "@higher-order/account/withAuthGuard";
+import { Button } from "@components/ui/button";
+import { Link } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@components/ui/dialog";
+import { Input } from "@components/ui/input";
+import { Label } from "@components/ui/label";
 
 const breadcrumbItems = [
   { title: "Resource Management", link: "/admin/resources" },
@@ -12,7 +25,45 @@ const LearningsAdmin = () => {
   return (
     <AdminOutletContainer className="container mx-auto py-10 ">
       <BreadCrumb items={breadcrumbItems} />
-      <h2 className="text-3xl font-bold tracking-tight">Learning Materials</h2>
+      <div className="flex justify-between items-center">
+        <h2 className="text-3xl font-bold tracking-tight">
+          Learning Materials
+        </h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>+Add</Button>
+          </DialogTrigger>
+
+          {/* modal */}
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Add Learning Material</DialogTitle>
+              <DialogDescription>
+                Create the title of your new learning material first and edit it
+                in drafts section. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4">
+              <div className="flex-col gap-4">
+                <Label htmlFor="title" className="text-right">
+                  Title
+                </Label>
+                <Input
+                  id="title"
+                  placeholder="insert title of your material"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button variant="outline">Back</Button>
+              <Link to="/admin/resource/learnings-draft">
+                <Button type="submit">Save Draft</Button>
+              </Link>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
       <p className="text-sm text-muted-foreground">
         Manage all learning materials.
       </p>

5cafa2b7e52290467d05d4b83567841aed14da0e
vince-dev023 <jan.vincentneal04@gmail.com>
2024-02-08 22:25:01 +0800

    second push vince

--- src/pages/user/blog/blogs-data.ts
+++ src/pages/user/blog/blogs-data.ts
@@ -3,9 +3,10 @@ export const blogsData = [
     title: "Permaculture Workshop Day 1",
     desc: "Nagsama-sama ang mga lider ng mga Urban Farms mula sa iba't ibang distrito ng Quezon City upang talakayin ng PERMACULTURE sa loob ng 3 araw sa urban farm ng Quezon City University - San Bartolome Campus kasama ang ating Guest Speaker na si Mr. Sixto Bereber na isang Permaculture Practitioner. Ayon sa kanya, ang Permakultura ay may disensyo na nakaayon sa libreng enerhiya at nakasandal sa ugnayan ng samut saring buhay. Bawat bahagi ay parte ng isang kabuuan na sumesentro sa pagpapanatiling buhay ang lupa. Layunin din ng aktibidad na ito na magkaroon ng organisasyon na magpapalaganap ng permakultura sa ating syudad upang makatulong sa pagiging sustenable at makakalikasang gawain. Ang aktibidad na ito ang pinangunahan ng Quezon City University - Center for Urban Agriculture and Innovation  (QCU-CUAI) sa pakikipagtulungan ng Joy of Urban Farming at Public Employment Service Office. #JoyofUrbanFarming #CUAI #QCU #PESO #UrbanAgriculture #Permaculture #Sustainability",
     img: "https://scontent.fmnl30-1.fna.fbcdn.net/v/t39.30808-6/394505940_305783188873991_2325097841943704071_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeH8I3TbIKggmem1adAQmPDXdpjkz_0x14V2mOTP_THXhU39YSM4vYUpHwgnHoFiQQcnexY0ni0kB68pp90sUUOw&_nc_ohc=UvQ5XLtFbbIAX8rGE-d&_nc_ht=scontent.fmnl30-1.fna&oh=00_AfAkpjHE-Pqe9Li1Fsoe74xdBImxXfUN-AxrMUUd3lAYRA&oe=65C163E6",
-    date: "2023-10-18T08:00:00",
+    date: "2023-10-18",
     blogId: "1",
-    category: "initiatives"
+    category: "Initiative",
+    isFeatured: true
   },
   {
     title: "Permaculture Workshop Day 2 10-19",
@@ -13,7 +14,8 @@ export const blogsData = [
     img: "https://scontent.fmnl30-1.fna.fbcdn.net/v/t39.30808-6/393968793_305856862199957_5750232922666883873_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeEMBlaNO4Jwcgw_JrPJeSi9UCkbiBDPc15QKRuIEM9zXo4PxbxGphhKrgUAXHNR1gzPwjUbQBuhPjk9VPSQwAa0&_nc_ohc=ppGMurw8H34AX9wd-ib&_nc_ht=scontent.fmnl30-1.fna&oh=00_AfC7yT3xONCLJeYCy2IWTfmnaha7-f109MEiwFr-EXuJCQ&oe=65C07901",
     date: "2023-10-19T08:00:00",
     blogId: "2",
-    category: "news"
+    category: "Initiative",
+    isFeatured: true
   },
   {
     title: "Permaculture Workshop Day 3",
@@ -21,7 +23,8 @@ export const blogsData = [
     img: "https://scontent.fmnl30-3.fna.fbcdn.net/v/t39.30808-6/394355967_305944588857851_7558847211395907860_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeFN5kmcVv-aeK07n4e6A_Z-QHGvsLdtd0tAca-wt213S8cMgYWOztyoxhSIoH3TLwAu7HVa6Ab0cxSuK1lY0I32&_nc_ohc=9SWSv6JKp8sAX-Ly7mY&_nc_ht=scontent.fmnl30-3.fna&oh=00_AfAOxidNTJoEfCNwmMsrGpRQx_1nNDkdG5LP0WkWgX2cHA&oe=65C0883E",
     date: "2023-10-20T08:00:00",
     blogId: "3",
-    category: "initiatives"
+    category: "Initiative",
+    isFeatured: true
   },
   {
     title:
@@ -30,7 +33,8 @@ export const blogsData = [
     img: "https://scontent.fmnl30-1.fna.fbcdn.net/v/t39.30808-6/352791541_230701053048872_1221509448153532647_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a73e89&_nc_eui2=AeHdDVhQ8b5gmZttK5YIXOv-P6Jr_75yhN0_omv_vnKE3ZmMjNUOFzQuKzp9gdIqk0gUxPzwKYng5B2qLcvlS90v&_nc_ohc=OPdQ49aJWyIAX-LZYIz&_nc_ht=scontent.fmnl30-1.fna&oh=00_AfDw6IOui6Ww9GOOSE1iEmc8NmPg-bA1eTCrQjF-xRhSqQ&oe=65C1139A",
     date: "2023-06-07T08:00:00",
     blogId: "4",
-    category: "news"
+    category: "News",
+    isFeatured: true
   },
   {
     title: "Farm Visit at Netafim-Netaphils Demo Farm",
@@ -38,14 +42,26 @@ export const blogsData = [
     img: "https://scontent.fmnl30-1.fna.fbcdn.net/v/t39.30808-6/352791541_230701053048872_1221509448153532647_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a73e89&_nc_eui2=AeHdDVhQ8b5gmZttK5YIXOv-P6Jr_75yhN0_omv_vnKE3ZmMjNUOFzQuKzp9gdIqk0gUxPzwKYng5B2qLcvlS90v&_nc_ohc=OPdQ49aJWyIAX-LZYIz&_nc_ht=scontent.fmnl30-1.fna&oh=00_AfDw6IOui6Ww9GOOSE1iEmc8NmPg-bA1eTCrQjF-xRhSqQ&oe=65C1139A",
     date: "2023-06-05T08:00:00",
     blogId: "5",
-    category: "initiatives"
+    category: "Initiative",
+    isFeatured: true
   },
+  {
+    title: "URBAN FARMING SEMINAR AT PM CONNECT GLOBAL TEAM INC.",
+    desc: "Mga grupo naman sa simbahan at mga pastor ang binabaan ng Center for Urban Agriculture and Innovation (CUAI) upang makapagbahagi ng mga kaalaman tungkol sa Urban Farming sa PM Connect Global Team Inc. Mayroon silang garden sa kani-kanilang mga bakuran at nais mapalawak ang kaalaman upang mas maging produktibo. Tinalakay dito ang tamang pagtatanim mula sa pagpupunla hanggang sa pag-harvest. Patuloy na dumadami ang sumusuporta sa Urban Agriculture Program ng Quezon City sa pamamagitan ng Joy of Urban Farming. Ang programa na ito ay naglalayong maging self-sufficient ang Quezon City kahit magkaroon ng anumang krisis. Marami nang urban farms sa Quezon City at patuloy na nadadagdagan sapagkat nakikita ang kahalagahan nito ngayong nagtataasan ang presyo ng mga bilihin. #JoyofUrbanFarming #cuai #qcu #UrbanAgriculture",
+    img: "https://scontent.fmnl30-1.fna.fbcdn.net/v/t39.30808-6/345176410_2012787345731635_943294003493432896_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeG6LDO7XdM7364kFEkPOfQ6Ps8Rc6JHDEQ-zxFzokcMREChnIsKHvLQinAc2H5mXQRYCROnvPxkbNXYSQNVgVuH&_nc_ohc=ZLOmYx3FwbAAX_iYmZh&_nc_ht=scontent.fmnl30-1.fna&oh=00_AfAtb_VLVL2jTS43HLl8f_0Nh1L0cT8iSGMmen3NxSiV2Q&oe=65C0ACC2",
+    date: "2023-10-19T08:00:00",
+    blogId: "6",
+    category: "News",
+    isFeatured: true
+  },
+
   {
     title: "URBAN FARMING SEMINAR AT PM CONNECT GLOBAL TEAM INC. 10-20",
     desc: "Mga grupo naman sa simbahan at mga pastor ang binabaan ng Center for Urban Agriculture and Innovation (CUAI) upang makapagbahagi ng mga kaalaman tungkol sa Urban Farming sa PM Connect Global Team Inc. Mayroon silang garden sa kani-kanilang mga bakuran at nais mapalawak ang kaalaman upang mas maging produktibo. Tinalakay dito ang tamang pagtatanim mula sa pagpupunla hanggang sa pag-harvest. Patuloy na dumadami ang sumusuporta sa Urban Agriculture Program ng Quezon City sa pamamagitan ng Joy of Urban Farming. Ang programa na ito ay naglalayong maging self-sufficient ang Quezon City kahit magkaroon ng anumang krisis. Marami nang urban farms sa Quezon City at patuloy na nadadagdagan sapagkat nakikita ang kahalagahan nito ngayong nagtataasan ang presyo ng mga bilihin. #JoyofUrbanFarming #cuai #qcu #UrbanAgriculture",
     img: "https://scontent.fmnl30-1.fna.fbcdn.net/v/t39.30808-6/345176410_2012787345731635_943294003493432896_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeG6LDO7XdM7364kFEkPOfQ6Ps8Rc6JHDEQ-zxFzokcMREChnIsKHvLQinAc2H5mXQRYCROnvPxkbNXYSQNVgVuH&_nc_ohc=ZLOmYx3FwbAAX_iYmZh&_nc_ht=scontent.fmnl30-1.fna&oh=00_AfAtb_VLVL2jTS43HLl8f_0Nh1L0cT8iSGMmen3NxSiV2Q&oe=65C0ACC2",
     date: "2023-10-20T08:00:00",
-    blogId: "6",
-    category: "news"
-  },
+    blogId: "7",
+    category: "News",
+    isFeatured: true
+  }
 ];

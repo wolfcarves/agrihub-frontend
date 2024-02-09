af29d12156665f51b2a5325d7c2065743cafeba0
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-02-10 00:04:48 +0800

    feat:  learning material create resource

---
+++ src/components/admin/form/learning-resource-form/add-learning-resource-form.tsx
@@ -0,0 +1,166 @@
+import React, { useState } from "react";
+import { Card } from "@components/ui/card";
+import { Label } from "../../../ui/label";
+import { Input } from "../../../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@components/ui/select";
+import { Textarea } from "../../../ui/textarea";
+import CoverImageUpload from "../../../ui/custom/image/cover-image-input";
+import { Button } from "../../../ui/button";
+import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { addResourceSchema } from "./schema";
+import { NewLearningResource } from "../../../../api/openapi";
+import useLearningCreateResource from "../../../../hooks/api/post/useLearningCreateResource";
+import { Form, FormField } from "../../../ui/form";
+import Dropzone from "../../../user/community/dropzone/dropzone";
+import { toast } from "sonner";
+interface AddLearningInterfaceProps {
+  setHide: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const AddLearningResourceForm: React.FC<AddLearningInterfaceProps> = ({
+  setHide
+}) => {
+  const [selectedType, setSelectedType] = useState<string | null>(null);
+  const { learningsId } = useParams();
+
+  const form = useForm<NewLearningResource>({
+    resolver: zodResolver(addResourceSchema),
+    mode: "onBlur"
+  });
+
+  //edit
+  const { mutateAsync: createResourceMutate, isLoading: isResourceLoading } =
+    useLearningCreateResource();
+
+  const handleSubmitForm = async (data: NewLearningResource) => {
+    const compiledData: NewLearningResource = {
+      name: data.name,
+      description: data.description,
+      resource: data.resource,
+      type: data.type,
+      image: data.image
+    };
+    console.log(compiledData);
+
+    try {
+      await createResourceMutate({
+        id: learningsId || "",
+        formData: compiledData
+      });
+      toast.success("Resource Added Successfully!");
+      setHide(false);
+    } catch (e: any) {
+      toast.error(e.body.message);
+    }
+  };
+  console.log(form.formState.errors);
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmitForm)}
+        encType="multipart/form-data"
+      >
+        <Card className="p-4 mb-4">
+          <div className="flex flex-wrap justify-between items-end gap-4 mb-8">
+            <h2 className="text-sm font-bold tracking-tight">
+              New Resource Form
+            </h2>
+            <div className="w-full flex justify-between gap-4">
+              <div className="grid w-full max-w-[46rem] items-center gap-1.5">
+                <Label>Title</Label>
+                <Input
+                  type="text"
+                  placeholder="Input resource title"
+                  {...form.register("name")}
+                />
+              </div>
+
+              <div className="grid w-full max-w-[10rem] items-center gap-1.5">
+                <Label>Type</Label>
+                <FormField
+                  control={form.control}
+                  name="type"
+                  render={({ field }) => (
+                    <Select
+                      onValueChange={newValue => {
+                        field.onChange(newValue);
+                        setSelectedType(newValue);
+                      }}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger>
+                        <SelectValue
+                          className=" uppercase"
+                          placeholder="Select Type.."
+                        />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="image">Image</SelectItem>
+                        <SelectItem value="video">Video</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  )}
+                />
+              </div>
+            </div>
+
+            <div className="grid w-full items-center gap-1.5">
+              <Label>Description</Label>
+              <Textarea
+                placeholder="insert resource desc"
+                className="active:border-green-500"
+                {...form.register("description")}
+              />
+            </div>
+            {/* for image upload  */}
+            {selectedType === "image" && (
+              <div className="grid w-full items-center gap-1.5">
+                <Label>Upload Image</Label>
+                <FormField
+                  control={form.control}
+                  name="image"
+                  render={() => (
+                    <Dropzone
+                      onChange={value => {
+                        form.setValue("image", value);
+                      }}
+                    />
+                  )}
+                />
+              </div>
+            )}
+
+            {/* for video upload */}
+            {selectedType === "video" && (
+              <div className="grid w-full items-center gap-1.5">
+                <Label>Video Source</Label>
+                <Input
+                  type="text"
+                  placeholder="Input video link"
+                  {...form.register("resource")}
+                />
+              </div>
+            )}
+          </div>
+          <div className="flex justify-end mt-4 gap-4">
+            <div className="flex gap-4">
+              <Button type="submit" variant="secondary">
+                Save
+              </Button>
+            </div>
+          </div>
+        </Card>
+      </form>
+    </Form>
+  );
+};
+
+export default AddLearningResourceForm;
---
+++ src/components/admin/form/learning-resource-form/learning-resource.tsx
@@ -0,0 +1,189 @@
+import React from "react";
+import { Input } from "@components/ui/input";
+import { Label } from "@components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@components/ui/select";
+import { Button } from "@components/ui/button";
+import CoverImageUpload from "@components/ui/custom/image/cover-image-input";
+import { useState } from "react";
+import { Textarea } from "@components/ui/textarea";
+import { Card } from "@components/ui/card";
+import { useParams } from "react-router-dom";
+import useGetLearningDraftView from "../../../../hooks/api/get/useGetLearningDraftView";
+import AddLearningResourceForm from "./add-learning-resource-form";
+import useDeleteLearningResource from "../../../../hooks/api/delete/useDeleteLearningResource";
+import { toast } from "sonner";
+import { Checkbox } from "../../../ui/checkbox";
+import usePutLearningFeatured from "../../../../hooks/api/put/usePutLearningFeatured";
+
+const LearningResourceForm = () => {
+  const { learningsId } = useParams();
+  const [hide, setHide] = useState<boolean>(false);
+  const { data: LearningData } = useGetLearningDraftView(learningsId || "");
+
+  const [resource, setResource] = React.useState(1);
+  const handleAddResource = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setResource(prevResource => Math.min(100, prevResource + 1));
+  };
+  const handleDeleteResource = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setResource(prevResource => Math.max(1, prevResource - 1));
+  };
+
+  // resource edit
+  const [isEditingResource, setIsEditingResource] = useState(false);
+  const handleEditingResource = () => {
+    setIsEditingResource(true);
+  };
+  const handleSaveResource = () => {
+    setIsEditingResource(false);
+  };
+
+  // type upload
+  const [selectedType, setSelectedType] = useState<string | null>(null);
+  const handleTypeChange = (value: string) => {
+    console.log("Selected type:", value);
+    setSelectedType(value);
+  };
+
+  const [selectedImages, setSelectedImages] = useState<File[]>([]);
+  const handleImageChange = (files: Blob[]) => {
+    setSelectedImages(files as File[]);
+  };
+
+  const { mutateAsync: deleteResource } = useDeleteLearningResource();
+  const handleDelete = async (id: string) => {
+    await deleteResource(id);
+    toast.success("Resource Deleted Successfully!");
+  };
+
+  const { mutateAsync: putFeatured } = usePutLearningFeatured();
+  const handleIsFeatured = async (id: string) => {
+    await putFeatured({ materialId: learningsId || "", id: id });
+    toast.success("Featured Successfully!");
+  };
+  return (
+    <div>
+      {/* add resource */}
+      <div className="flex justify-between items-center mt-4 mb-2">
+        <h2 className="text-md font-bold tracking-tight">Resource</h2>
+      </div>
+
+      {/* resource form */}
+      {LearningData?.learning_resource?.map((resource, index) => (
+        <Card key={index} className="p-4 mb-4">
+          <div className="flex flex-wrap justify-between items-end gap-4 mb-8">
+            <h2 className="text-sm font-bold tracking-tight">
+              Resource {index + 1}
+            </h2>
+            <div className="w-full flex justify-between gap-4">
+              <div className="grid w-full max-w-[46rem] items-center gap-1.5">
+                <Label>Title</Label>
+                <Input
+                  type="text"
+                  value={resource.name}
+                  placeholder="Input resource title"
+                  disabled={true}
+                />
+              </div>
+
+              <div className="grid w-full max-w-[10rem] items-center gap-1.5">
+                <Label>Type</Label>
+                <Input
+                  type="text"
+                  className=" capitalize"
+                  value={resource.type}
+                  placeholder="Type"
+                  disabled={true}
+                />
+              </div>
+            </div>
+
+            <div className="grid w-full items-center gap-1.5">
+              <Label>Description</Label>
+              <Textarea
+                placeholder="insert resource desc"
+                className="active:border-green-500"
+                value={resource.description || ""}
+                disabled={true}
+              />
+            </div>
+            {/* Source  */}
+            {resource.type === "video" && (
+              <div className="grid w-full items-center gap-1.5">
+                <div>
+                  <Label>Video Source</Label>
+                  <Input
+                    type="text"
+                    value={resource.resource}
+                    placeholder="Input resource link"
+                    disabled={!isEditingResource}
+                  />
+                </div>
+              </div>
+            )}
+            {resource.type === "image" && (
+              <div className="grid w-full items-center gap-1.5">
+                <div>
+                  <Label>Image Uploaded</Label>
+                  <img
+                    src={resource.resource}
+                    alt="Uploaded"
+                    className="h-64 w-full rounded-lg object-cover object-center border"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="flex justify-between mt-4 gap-4">
+            <div className="flex items-center space-x-2">
+              <Checkbox
+                checked={resource.is_featured}
+                onCheckedChange={() => handleIsFeatured(resource.id)}
+              />
+              <label className="text-sm font-medium leading-none">
+                Featured
+              </label>
+            </div>
+
+            <div className="flex gap-4">
+              <Button
+                variant="destructive"
+                onClick={() => handleDelete(resource.id)}
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+        </Card>
+      ))}
+      <>
+        {hide && (
+          <>
+            <hr className="my-8" />
+            <AddLearningResourceForm setHide={setHide} />
+          </>
+        )}
+      </>
+      {hide ? (
+        <div className="flex justify-end">
+          <Button variant={"destructive"} onClick={() => setHide(false)}>
+            Cancel
+          </Button>
+        </div>
+      ) : (
+        <div className="flex justify-end">
+          <Button onClick={() => setHide(true)}>Add more source</Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LearningResourceForm;

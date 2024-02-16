a4021290c09404536166a9c495a2b4c9c6b9b166
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-16 09:36:42 +0800

    feat: articles

--- src/pages/user/article/article.tsx
+++ src/pages/user/article/article.tsx
@@ -1,9 +1,66 @@
 import thumnail from "@assets/images/thumnail.svg";
 import dp from "@icons/center-logo.svg";
 import { PiEyeBold } from "react-icons/pi";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import {
+  articlesData,
+  articleImagesData,
+  ArticleSchema,
+  articleCreditsData
+} from "../../../constants/data";
+import { Link } from "react-router-dom";
+import { formatDate } from "@components/lib/utils";
+import { Button } from "@components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from "@components/ui/carousel";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from "@components/ui/accordion";
+
 const Article = () => {
+  const { articleId } = useParams();
+
+  // State to track whether to show all related articles
+  const [showAllRelated, setShowAllRelated] = useState(false);
+
+  const currentArticle = articlesData.find(
+    article => article.articleId === articleId
+  );
+  const currentArticleImages = articleImagesData.filter(
+    image => image.articleId === articleId
+  );
+  const getRelatedArticles = (): ArticleSchema[] => {
+    return articlesData.filter(article => article.articleId !== articleId);
+  };
+
+  const findFirstImage = (articleId: string): string | undefined => {
+    const firstImage = articleImagesData.find(
+      image => image.articleId === articleId
+    );
+    return firstImage ? firstImage.image : undefined;
+  };
+
+  const relatedArticles = getRelatedArticles();
+
+  // Limit related articles to 3 initially or show all if showAllRelated is true
+  const displayedRelatedArticles = showAllRelated
+    ? relatedArticles
+    : relatedArticles.slice(0, 3);
+
+  const credits = articleCreditsData.filter(
+    credit => credit.articleId === articleId
+  );
   return (
-    <div className="pt-3 pb-8 px-[5rem] ">
+    <div className="mt-3 mb-8 mx-16 ">
       <div className="flex justify-between items-center">
         <div className="text-[#188B16] mb-3">
           Farming / Agriculture / Urban Farming
@@ -12,136 +69,174 @@ const Article = () => {
           <PiEyeBold size={18} /> 4.6K
         </div>
       </div>
-      <img
-        className="w-full h-[20rem] object-cover object-center rounded-2xl"
-        src={thumnail}
-      />
+      <Carousel className="w-full">
+        <CarouselContent>
+          {currentArticleImages.map((image, index) => (
+            <CarouselItem key={index}>
+              <section className="my-8">
+                <div className="max-w-screen-xl mx-auto md:px-8">
+                  <div className="items-center gap-x-12 sm:px-4 md:px-0 lg:flex">
+                    <div className="flex-1 sm:hidden lg:block">
+                      <img
+                        src={image.image}
+                        className="md:max-w-lg max-h-96 min-h-96 object-cover sm:rounded-lg"
+                        alt={image.title}
+                      />
+                    </div>
+                    <div className="max-w-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl">
+                      <h3 className="text-green-600 font-semibold">
+                        Photograph by {image.photographer}
+                      </h3>
+                      <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                        {image.title}
+                      </p>
+                      <p className="mt-3 text-gray-600">{image.description}</p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
       <div className="flex justify-center my-5 items-center gap-2">
-        <img className="h-[2.3rem]" src={dp} />
+        <img className="h-[2.3rem]" src={dp as unknown as string} />
         <p className=" font-semibold">Center for Urban Agriculture</p>
         <p className=" font-semibold">|</p>
-        <p className=" font-semibold">November 29, 2023</p>
+        <p className=" font-semibold">
+          {formatDate(currentArticle?.updatedAt)}
+        </p>
       </div>
       <div className="flex justify-center mb-4">
         <h1 className=" font-semibold">
-          “Empowering Agriculture on each Highly Urbanized Districts”
+          “{currentArticle?.title || "Title not found"}”
         </h1>
       </div>
       <div>
         <p className=" leading-loose font-medium">
-          The empowerment of agriculture in urbanized cities represents a
-          transformative approach to addressing food security, sustainability,
-          and community resilience. As urban areas expand, the integration of
-          agriculture into the urban fabric becomes increasingly vital. This
-          empowerment involves leveraging innovative technologies, sustainable
-          practices, and community engagement to enhance the productivity and
-          viability of urban agriculture....The empowerment of agriculture in
-          urbanized cities represents a transformative approach to addressing
-          food security, sustainability, and community resilience. As urban
-          areas expand, the integration of agriculture into the urban fabric
-          becomes increasingly vital. This empowerment involves leveraging
-          innovative technologies, sustainable practices, and community
-          engagement to enhance the productivity and viability of urban
-          agriculture....The empowerment of agriculture in urbanized cities
-          represents a transformative approach to addressing food security,
-          sustainability, and community resilience. As urban areas expand, the
-          integration of agriculture into the urban fabric becomes increasingly
-          vital. This empowerment involves leveraging innovative technologies,
-          sustainable practices, and community engagement to enhance the
-          productivity and viability of urban agriculture....The empowerment of
-          agriculture in urbanized cities represents a transformative approach
-          to addressing food security, sustainability, and community resilience.
-          As urban areas expand, the integration of agriculture into the urban
-          fabric becomes increasingly vital. This empowerment involves
-          leveraging innovative technologies, sustainable practices, and
-          community engagement to enhance the productivity and viability of
-          urban agriculture....The empowerment of agriculture in urbanized
-          cities represents a transformative approach to addressing food
-          security, sustainability, and community resilience. As urban areas
-          expand, the integration of agriculture into the urban fabric becomes
-          increasingly vital. This empowerment involves leveraging innovative
-          technologies, sustainable practices, and community engagement to
-          enhance the productivity and viability of urban agriculture....The
-          empowerment of agriculture in urbanized cities represents a
-          transformative approach to addressing food security, sustainability,
-          and community resilience. As urban areas expand, the integration of
-          agriculture into the urban fabric becomes increasingly vital. This
-          empowerment involves leveraging innovative technologies, sustainable
-          practices, and community engagement to enhance the productivity and
-          viability of urban agriculture....
+          {currentArticle?.description || "Description not found"}
         </p>
       </div>
+
+      {/* credits */}
+      <Accordion type="multiple" className="max-w-3xl mx-auto">
+        <AccordionItem value="credits">
+          <AccordionTrigger className="decoration-green-400">
+            <h3 className="my-4 font-poppins-semibold">Credits</h3>
+          </AccordionTrigger>
+          {credits.map((credit, index) => (
+            <AccordionContent key={index}>
+              <p className="text-gray-700 font-semibold sm:text-md">
+                {credit.name}
+              </p>
+              <p className="text-green-600 mb-3">{credit.title}</p>
+            </AccordionContent>
+          ))}
+        </AccordionItem>
+
+        <AccordionItem value="user-permission">
+          <AccordionTrigger className=" decoration-green-400">
+            <h3 className="my-4 font-poppins-semibold">User Permissions</h3>
+          </AccordionTrigger>
+          <AccordionContent>
+            <span className="text-md">
+              For information on user permissions, please read our Terms of
+              Service. If you have questions about how to cite anything on our
+              website in your project or classroom presentation, please contact
+              your teacher. They will best know the preferred format. When you
+              reach out to them, you will need the page title, URL, and the date
+              you accessed the resource.
+            </span>
+            <br />
+            <div className="mb-2 mt-4">
+              <span className="text-lg">
+                <b>Media</b>
+              </span>
+              <br />
+              <span className="text-md">
+                If a media asset is downloadable, a download button appears in
+                the corner of the media viewer. If no button appears, you cannot
+                download or save the media.
+              </span>
+            </div>
+            <br />
+            <div className="my-2">
+              <span className="text-lg">
+                <b>Text</b>
+              </span>
+              <br />
+              <span className="text-md">
+                Text on this page is printable and can be used according to our{" "}
+                <Link
+                  to="/terms-condition"
+                  className="underline decoration-solid decoration-green-400 hover:decoration-gray-950 underline-offset-[3px]"
+                >
+                  Terms of Service
+                </Link>
+                .
+              </span>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
       <div className="flex justify-center mt-14 mb-8">
-        <h2 className=" font-bold">Related Articles</h2>
+        <h2 className="font-bold">Related Articles</h2>
       </div>
-      <div className="grid grid-cols-10 gap-10">
-        <div className=" col-span-5">
-          <img
-            className="w-full h-[15rem] object-cover object-center rounded-2xl"
-            src={thumnail}
-          />
-          <div className="flex justify-center text-sm my-2 items-center gap-2">
-            <img className="h-[1.5rem]" src={dp} />
-            <p className=" font-semibold">Center for Urban Agriculture</p>
-            <p className=" font-semibold">|</p>
-            <p className=" font-semibold">November 29, 2023</p>
-          </div>
-          <div className="flex justify-center text-start mb-2">
-            <h3 className=" font-semibold line-clamp-2">
-              “Empowering Agriculture on each Highly Urbanized Districts”
-            </h3>
-          </div>
-          <div>
-            <p className=" leading-tight font-medium line-clamp-5">
-              The empowerment of agriculture in urbanized cities represents a
-              transformative approach to addressing food security,
-              sustainability, and community resilience. As urban areas expand,
-              the integration of agriculture into the urban fabric becomes
-              increasingly vital. This empowerment involves leveraging
-              innovative technologies, sustainable practices, and community
-              engagement to enhance the productivity and viability of urban
-              agriculture. The empowerment of agriculture in urbanized cities
-              represents a transformative approach to addressing food security,
-              sustainability, and community resilience. As urban areas expand,
-              the integration of agriculture into the urban fabric becomes
-              increasingly vital.
-            </p>
-          </div>
-        </div>
-        <div className=" col-span-5">
-          <img
-            className="w-full h-[15rem] object-cover object-center rounded-2xl"
-            src={thumnail}
-          />
-          <div className="flex justify-center text-sm my-2 items-center gap-2">
-            <img className="h-[1.5rem]" src={dp} />
-            <p className=" font-semibold">Center for Urban Agriculture</p>
-            <p className=" font-semibold">|</p>
-            <p className=" font-semibold">November 29, 2023</p>
-          </div>
-          <div className="flex justify-center text-start mb-2">
-            <h3 className=" font-semibold line-clamp-2">
-              “Empowering Agriculture on each Highly Urbanized Districts”
-            </h3>
-          </div>
-          <div>
-            <p className=" leading-tight font-medium line-clamp-5">
-              The empowerment of agriculture in urbanized cities represents a
-              transformative approach to addressing food security,
-              sustainability, and community resilience. As urban areas expand,
-              the integration of agriculture into the urban fabric becomes
-              increasingly vital. This empowerment involves leveraging
-              innovative technologies, sustainable practices, and community
-              engagement to enhance the productivity and viability of urban
-              agriculture. The empowerment of agriculture in urbanized cities
-              represents a transformative approach to addressing food security,
-              sustainability, and community resilience. As urban areas expand,
-              the integration of agriculture into the urban fabric becomes
-              increasingly vital.
-            </p>
-          </div>
-        </div>
+
+      <div className="flex flex-wrap gap-8 justify-center my-8">
+        {displayedRelatedArticles.map((article: ArticleSchema) => {
+          const firstImage = findFirstImage(article.articleId);
+
+          return (
+            <Link
+              to={`/articles/view/${article.title}/${article.articleId}`}
+              key={article.articleId}
+            >
+              <div className="max-w-sm mb-4">
+                <img
+                  className="w-full h-[15rem] object-cover object-center rounded-2xl"
+                  src={firstImage || ""}
+                  alt={article.title}
+                />
+                <div className="flex justify-center text-sm my-2 items-center gap-2">
+                  <img
+                    className="h-[1.5rem]"
+                    src={dp as unknown as string}
+                    alt="Logo"
+                  />
+                  <p className="font-semibold">Center for Urban Agriculture</p>
+                  <p className="font-semibold">|</p>
+                  <p className="font-semibold">
+                    Updated at {formatDate(article.updatedAt)}
+                  </p>
+                </div>
+                <div className="flex justify-center text-start mb-2">
+                  <h3 className="font-semibold line-clamp-2">
+                    {article.title}
+                  </h3>
+                </div>
+                <div>
+                  <p className="leading-tight font-medium line-clamp-5">
+                    {article.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
+      {!showAllRelated && (
+        <Button
+          variant="outline"
+          className="w-full"
+          onClick={() => setShowAllRelated(true)}
+        >
+          See More
+        </Button>
+      )}
     </div>
   );
 };
--- src/pages/user/article/articles.tsx
+++ src/pages/user/article/articles.tsx
@@ -1,9 +1,115 @@
-/*
-  path - /articles
-*/
+import React, { useState } from "react";
+import dp from "@icons/center-logo.svg";
+import {
+  articlesData,
+  articleImagesData,
+  ArticleSchema
+} from "../../../constants/data";
+import { formatDate } from "@components/lib/utils";
+import { Link } from "react-router-dom";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationPrevious,
+  PaginationNext
+} from "@components/ui/Pagination";
 
 const Articles = () => {
-  return <div>Articles Page</div>;
+  const itemsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const findFirstImage = (articleId: string): string | undefined => {
+    const firstImage = articleImagesData.find(
+      image => image.articleId === articleId
+    );
+    return firstImage ? firstImage.image : undefined;
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedArticles = articlesData.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(articlesData.length / itemsPerPage);
+
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  return (
+    <div className="m-8">
+      <h3 className="mb-3">Latest Articles</h3>
+      <div className="flex flex-wrap gap-8 justify-center my-8">
+        {paginatedArticles.map((article: ArticleSchema) => {
+          const firstImage = findFirstImage(article.articleId);
+
+          return (
+            <Link
+              to={`/articles/view/${article.title}/${article.articleId}`}
+              key={article.articleId}
+            >
+              <div className="max-w-sm mb-4">
+                <img
+                  className="w-full h-[15rem] object-cover object-center rounded-2xl"
+                  src={firstImage || ""}
+                  alt={article.title}
+                />
+                <div className="flex justify-center text-sm my-2 items-center gap-2">
+                  <img
+                    className="h-[1.5rem]"
+                    src={dp as unknown as string}
+                    alt="Logo"
+                  />
+                  <p className="font-semibold">Center for Urban Agriculture</p>
+                  <p className="font-semibold">|</p>
+                  <p className="font-semibold">
+                    Updated at {formatDate(article.updatedAt)}
+                  </p>
+                </div>
+                <div className="flex justify-center text-start mb-2">
+                  <h3 className="font-semibold line-clamp-2">
+                    {article.title}
+                  </h3>
+                </div>
+                <div>
+                  <p className="leading-tight font-medium line-clamp-5">
+                    {article.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationPrevious
+            disabled={currentPage === 1}
+            onClick={handlePreviousPage}
+          />
+          {[...Array(totalPages)].map((_, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink
+                isActive={index + 1 === currentPage}
+                onClick={() => setCurrentPage(index + 1)}
+              >
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationNext
+            disabled={currentPage === totalPages}
+            onClick={handleNextPage}
+          />
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
 };
 
 export default Articles;

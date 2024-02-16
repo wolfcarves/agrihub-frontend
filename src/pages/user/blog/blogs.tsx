d6a4535063727137387d94a25bd3274175043a09
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-16 09:37:16 +0800

    style: blogs

--- src/pages/user/blog/blogs.tsx
+++ src/pages/user/blog/blogs.tsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { formatDateTime } from "@components/lib/utils";
 import { Link } from "react-router-dom";
 import { ellipsis } from "./blog";
 import { blogData, blog_image } from "../../../constants/data";
 
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationPrevious,
+  PaginationNext
+} from "@components/ui/Pagination";
+
 const Blogs = () => {
+  const itemsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedData = blogData.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(blogData.length / itemsPerPage);
+
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
   return (
     <div className="py-4 px-8">
       <h3 className="mb-3">Recent Blogs</h3>
-      <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 grid-rows-3 gap-5">
-        {blogData.map((item, index) => {
+      <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 grid-rows-2 gap-5">
+        {paginatedData.map((item, index) => {
           const correspondingImages = blog_image.filter(
             image => image.blogId === item.blogId
           );
           const thumbnail = correspondingImages.find(image => image.thumbnail);
           return (
-            <Link to={`/blogs/view/${item.blogId}`}>
-              <div key={index} className="group flex flex-col">
+            <Link to={`/blogs/view/${item.blogId}`} key={index}>
+              <div className="group flex flex-col">
                 <div className="max-h-370px max-w-750px">
                   <img
                     src={thumbnail ? thumbnail.image : "fallback_image_url"}
                     loading="lazy"
                     alt={item.title}
-                    className="w-full rounded-lg max-h-370px max-w-750px"
+                    className="w-full rounded-lg max-h-64 min-h-64 object-cover"
                   />
                 </div>
 
@@ -46,6 +72,29 @@ const Blogs = () => {
           );
         })}
       </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationPrevious
+            disabled={currentPage === 1}
+            onClick={handlePreviousPage}
+          />
+          {[...Array(totalPages)].map((_, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink
+                isActive={index + 1 === currentPage}
+                onClick={() => setCurrentPage(index + 1)}
+              >
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationNext
+            disabled={currentPage === totalPages}
+            onClick={handleNextPage}
+            className="my-8"
+          />
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };

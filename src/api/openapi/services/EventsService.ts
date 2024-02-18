a929c06267bab34847d8d093786ff8aff81a19d3
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-02-18 19:58:01 +0800

    chore: codegen

--- src/api/openapi/services/EventsService.ts
+++ src/api/openapi/services/EventsService.ts
@@ -335,4 +335,271 @@ pagination?: PaginationData;
         });
     }
 
+    /**
+     * Archive an event
+     * @returns any Event archived successfully
+     * @throws ApiError
+     */
+    public static deleteApiEventsArchive({
+id,
+}: {
+/**
+ * The ID of the event to be archived
+ */
+id: string,
+}): CancelablePromise<{
+message?: string;
+}> {
+        return __request(OpenAPI, {
+            method: 'DELETE',
+            url: '/api/events/archive/{id}',
+            path: {
+                'id': id,
+            },
+            errors: {
+                400: `Validation Error`,
+                401: `Unauthorized`,
+                404: `Not Found Error`,
+                500: `Server Error`,
+            },
+        });
+    }
+
+    /**
+     * Archive an event
+     * @returns any Event archived successfully
+     * @throws ApiError
+     */
+    public static putApiEventsUnarchive({
+id,
+}: {
+/**
+ * The ID of the event to be archived
+ */
+id: string,
+}): CancelablePromise<{
+message?: string;
+}> {
+        return __request(OpenAPI, {
+            method: 'PUT',
+            url: '/api/events/unarchive/{id}',
+            path: {
+                'id': id,
+            },
+            errors: {
+                400: `Validation Error`,
+                401: `Unauthorized`,
+                404: `Not Found Error`,
+                500: `Server Error`,
+            },
+        });
+    }
+
+    /**
+     * Retrieve a list of archived events
+     * @returns any Successful response with a list of archived events
+     * @throws ApiError
+     */
+    public static getApiEventsArchivedList({
+search,
+page,
+perpage,
+}: {
+/**
+ * Search keyword for events
+ */
+search?: string,
+/**
+ * Page number for pagination
+ */
+page?: string,
+/**
+ * Number of items per page
+ */
+perpage?: string,
+}): CancelablePromise<{
+data?: Array<EventDetails>;
+pagination?: PaginationData;
+}> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/events/archived/list',
+            query: {
+                'search': search,
+                'page': page,
+                'perpage': perpage,
+            },
+            errors: {
+                400: `Validation Error`,
+                401: `Unauthorized`,
+                404: `Not Found Error`,
+                500: `Server Error`,
+            },
+        });
+    }
+
+    /**
+     * Delete a draft event
+     * @returns any Event deleted successfully
+     * @throws ApiError
+     */
+    public static deleteApiEventsDeleteDraft({
+id,
+}: {
+/**
+ * The ID of the event to be archived
+ */
+id: string,
+}): CancelablePromise<{
+message?: string;
+}> {
+        return __request(OpenAPI, {
+            method: 'DELETE',
+            url: '/api/events/delete/draft/{id}',
+            path: {
+                'id': id,
+            },
+            errors: {
+                400: `Validation Error`,
+                401: `Unauthorized`,
+                404: `Not Found Error`,
+                500: `Server Error`,
+            },
+        });
+    }
+
+    /**
+     * Publish an event
+     * @returns any Event published successfully
+     * @throws ApiError
+     */
+    public static putApiEventsPublish({
+id,
+}: {
+/**
+ * The ID of the event to be archived
+ */
+id: string,
+}): CancelablePromise<{
+message?: string;
+}> {
+        return __request(OpenAPI, {
+            method: 'PUT',
+            url: '/api/events/publish/{id}',
+            path: {
+                'id': id,
+            },
+            errors: {
+                400: `Validation Error`,
+                401: `Unauthorized`,
+                404: `Not Found Error`,
+                500: `Server Error`,
+            },
+        });
+    }
+
+    /**
+     * Unpublish an event
+     * @returns any Event Unpublished successfully
+     * @throws ApiError
+     */
+    public static putApiEventsUnpublish({
+id,
+}: {
+/**
+ * The ID of the event to be Unpublished
+ */
+id: string,
+}): CancelablePromise<{
+message?: string;
+}> {
+        return __request(OpenAPI, {
+            method: 'PUT',
+            url: '/api/events/unpublish/{id}',
+            path: {
+                'id': id,
+            },
+            errors: {
+                400: `Validation Error`,
+                401: `Unauthorized`,
+                404: `Not Found Error`,
+                500: `Server Error`,
+            },
+        });
+    }
+
+    /**
+     * View details of an event
+     * @returns EventDetailsResponse Successful response
+     * @throws ApiError
+     */
+    public static getApiEventsPublished({
+id,
+}: {
+id: string,
+}): CancelablePromise<EventDetailsResponse> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/events/published/{id}',
+            path: {
+                'id': id,
+            },
+            errors: {
+                400: `Validation Error`,
+                401: `Unauthorized`,
+                404: `Not Found Error`,
+                500: `Server Error`,
+            },
+        });
+    }
+
+    /**
+     * Retrieve a list of published events
+     * @returns any Successful response with a list of published events
+     * @throws ApiError
+     */
+    public static getApiEventsPublishedList({
+search,
+page,
+perpage,
+filter,
+}: {
+/**
+ * Search keyword for events
+ */
+search?: string,
+/**
+ * Page number for pagination
+ */
+page?: string,
+/**
+ * Number of items per page
+ */
+perpage?: string,
+/**
+ * Filter criteria
+ */
+filter?: 'upcoming' | 'previous',
+}): CancelablePromise<{
+data?: Array<EventDetails>;
+pagination?: PaginationData;
+}> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/events/published/list',
+            query: {
+                'search': search,
+                'page': page,
+                'perpage': perpage,
+                'filter': filter,
+            },
+            errors: {
+                400: `Validation Error`,
+                401: `Unauthorized`,
+                404: `Not Found Error`,
+                500: `Server Error`,
+            },
+        });
+    }
+
 }

e2fe29b356884ecb0971078f733f976688cf7a68
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-19 17:34:49 +0800

    feat: adin header redirect to dashboard

--- src/components/ui/custom/header/user-header-menu.tsx
+++ src/components/ui/custom/header/user-header-menu.tsx
@@ -24,7 +24,7 @@ const UserHeaderMenu = () => {
   const { mutateAsync: deleteAuthData, isLoading } = useDeleteAuthMutate();
 
   const handleToAdmin = () => {
-    navigate(`/admin`);
+    navigate(`/admin/dashboard`);
   };
 
   return (

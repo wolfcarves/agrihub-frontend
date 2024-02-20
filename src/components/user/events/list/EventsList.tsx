2114da1607571d27a2aad02b47ca68ffffc90cfb
Rodel Crisosto <89376217+wolfcarves@users.noreply.github.com>
2024-02-20 20:44:13 +0800

    style : restyled events page (#153)

--- src/components/user/events/list/EventsList.tsx
+++ src/components/user/events/list/EventsList.tsx
@@ -10,7 +10,7 @@ const EventsList = () => {
   });
 
   return (
-    <>
+    <div className="min-h-[30rem] container">
       <div className="py-10 font-poppins-semibold text-black/70 border-b-2">
         <h3>Upcoming Events</h3>
       </div>
@@ -20,7 +20,7 @@ const EventsList = () => {
           return <EventsCard key={data.id} {...data} />;
         })}
       </div>
-    </>
+    </div>
   );
 };
 

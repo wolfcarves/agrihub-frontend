776fd4e686651ed7813b78ed01c17b09aa216559
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-02-21 21:32:19 +0800

    feat: blogs action buttons

---
+++ src/hooks/api/put/usePutBlogsPublish.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { BlogsService } from "@api/openapi";
+import { GET_BLOGS_PUBLISH_LIST } from "../get/useGetBlogsPublishList";
+
+const usePutBlogsPublishKey = () => "PUT_BLOGS_PUBLISH_KEY";
+
+export default function usePutBlogsPublish() {
+  const queryClient = useQueryClient();
+
+  return useMutation([usePutBlogsPublishKey()], {
+    async mutationFn(id: string) {
+      const response = await BlogsService.putApiBlogsPublish({
+        id
+      });
+
+      return response;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries([GET_BLOGS_PUBLISH_LIST()]);
+    }
+  });
+}

c6c39f8f6973e8605c07bdac1759d48ce18002e5
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-02-21 21:47:13 +0800

    feat: table publish blog

---
+++ src/components/admin/blogs/table/table-blogs-published/columns.tsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@components/ui/dropdown-menu";
+
+import { Link } from "react-router-dom";
+import { Blog } from "../../../../../api/openapi";
+import { format } from "date-fns";
+
+export const columns: ColumnDef<Blog>[] = [
+  {
+    accessorKey: "createdat",
+    header: "Created At",
+    cell: ({ row }) => format(new Date(row.original.createdat || ""), "PPP")
+  },
+  {
+    accessorKey: "updatedat",
+    header: "Updated At",
+    cell: ({ row }) => format(new Date(row.original.updatedat || ""), "PPP")
+  },
+  {
+    accessorKey: "title",
+    header: "Title"
+  },
+  {
+    accessorKey: "author",
+    header: "Author"
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => <div>{row.getValue("status")}</div>
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id || "")}
+            >
+              Copy blog ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <Link to={`/admin/resource/blogs/view/${payment.id}`}>
+              <DropdownMenuItem>View/update article</DropdownMenuItem>
+            </Link>
+            <Link to={`/blogs/view/${payment.id}`}>
+              <DropdownMenuItem>View article in page</DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    }
+  }
+];

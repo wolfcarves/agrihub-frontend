ac7b3ee0c3b57302509ef2ea418cdba4171ee1e6
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-02-22 15:52:39 +0800

    feat: blogs  table search

--- src/components/admin/blogs/table/table-blogs-published/table-blogs-published.tsx
+++ src/components/admin/blogs/table/table-blogs-published/table-blogs-published.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../../../../ui/input";
 import { DataTable } from "../../../../ui/custom/data-table/data-table";
 import { columns } from "./columns";
 import useGetBlogsPublishList from "../../../../../hooks/api/get/useGetBlogsPublishList";
+import useDebounce from "../../../../../hooks/utils/useDebounce";
 
 const TableBlogsPublished = () => {
-  const { data: blogsData } = useGetBlogsPublishList(
-    undefined,
-    undefined,
-    undefined
-  );
+  const [search, setSearch] = useState<string | undefined>("");
+  const { data: blogsData } = useGetBlogsPublishList(search, "1", "20");
+  const debouncedSearch = useDebounce((value: string) => {
+    setSearch(value);
+  }, 100);
   return (
     <div>
-      <Input placeholder="Search title..." className="max-w-sm my-4" />
+      <Input
+        placeholder="Search title..."
+        className="max-w-sm my-4"
+        value={search}
+        onChange={e => debouncedSearch(e.target.value)}
+      />
       <DataTable columns={columns} data={blogsData?.data || []} />
     </div>
   );

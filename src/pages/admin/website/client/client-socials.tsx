c2c27f11f85ac5efb5d3d9b9f7c7498cd0602f63
Rodel Crisosto <89376217+wolfcarves@users.noreply.github.com>
2024-02-24 14:00:35 +0800

    fix: black screen (#185)

--- src/pages/admin/website/client/client-socials.tsx
+++ src/pages/admin/website/client/client-socials.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Button } from "@components/ui/button";
 import { Card } from "@components/ui/card";
 import { Input } from "@components/ui/input";
-import { ComboboxSocialMedia } from "../components/combobox-social-media";
 import { FaTrashCan } from "react-icons/fa6";
 
 interface SocialMediaItem {
@@ -47,7 +46,6 @@ const ClientSocials = () => {
           socialMediaList.map(socialMedia => (
             <Card key={socialMedia.id} className="p-5 mb-4">
               <div className="flex flex-wrap sm:flex-nowrap w-full gap-4 items-center">
-                <ComboboxSocialMedia />
                 <Input
                   type="text"
                   id="link"

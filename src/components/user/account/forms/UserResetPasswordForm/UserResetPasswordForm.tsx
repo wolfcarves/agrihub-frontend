74fb38178223656b8b8be9fc79d3456e59a7344a
Cazcade <rodel.crisosto7@gmail.com>
2024-02-25 18:48:28 +0800

    fix : removed base prop in vite.config.js

--- src/components/user/account/forms/UserResetPasswordForm/UserResetPasswordForm.tsx
+++ src/components/user/account/forms/UserResetPasswordForm/UserResetPasswordForm.tsx
@@ -10,8 +10,6 @@ import { PiCheckFatBold } from "react-icons/pi";
 import Unauthorized from "@pages/user/common/unauthorized";
 
 const UserResetPasswordForm = () => {
-  return <>Testing for Production</>;
-
   const { register, formState, handleSubmit } = useForm<ResetPasswordType>({
     mode: "onSubmit",
     reValidateMode: "onChange",

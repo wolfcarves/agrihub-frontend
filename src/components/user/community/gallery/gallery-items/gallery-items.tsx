1d4b7eb16e84510692c65f4975f8d12aa0444edb
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-02-27 05:11:31 +0800

    feat: view img?

--- src/components/user/community/gallery/gallery-items/gallery-items.tsx
+++ src/components/user/community/gallery/gallery-items/gallery-items.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useGetFarmGalleryQuery from "../../../../../hooks/api/get/useGetFarmGalleryQuery";
 import useAuth from "../../../../../hooks/useAuth";
@@ -9,11 +9,21 @@ import {
 } from "../../../../ui/tooltip";
 import DeleteImageAlert from "../delete-image-modal/delete-image-alert";
 import useCommunityAutorization from "../../../../../hooks/utils/useCommunityAutorization";
+import ImgModal from "../../../../ui/custom/img-modal/Modal";
 
 const GalleryItems = () => {
   const { id } = useParams();
   const { data: farmGallery } = useGetFarmGalleryQuery(id || "");
   const { isAllowed, isMember } = useCommunityAutorization();
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleImageClick = (image: string) => {
+    setSelectedImage(image);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
   if (!farmGallery?.length || 0 > 0) {
     return (
       <div className="text-center">
@@ -21,6 +31,7 @@ const GalleryItems = () => {
       </div>
     );
   }
+
   return (
     <div className="flex gap-3 flex-wrap">
       {farmGallery?.map((gallery, i) => (
@@ -29,6 +40,7 @@ const GalleryItems = () => {
             <img
               className="h-[10rem] rounded hover:shadow-lg"
               src={gallery.imagesrc}
+              onClick={() => handleImageClick(gallery.imagesrc)}
             />
             {isMember && isAllowed && <DeleteImageAlert imageId={gallery.id} />}
           </TooltipTrigger>
@@ -37,6 +49,15 @@ const GalleryItems = () => {
           </TooltipContent>
         </Tooltip>
       ))}
+      {selectedImage && (
+        <ImgModal setModal={closeModal}>
+          <img
+            src={selectedImage}
+            alt="Full View"
+            className="max-w-full h-[20rem] max-h-full"
+          />
+        </ImgModal>
+      )}
     </div>
   );
 };

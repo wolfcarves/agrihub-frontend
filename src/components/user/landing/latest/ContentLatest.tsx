ba5f4be7d278e7f8629b2d917324ecfb035cffec
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-28 08:14:05 +0800

    feat: intigrate latest news blogs

--- src/components/user/landing/latest/ContentLatest.tsx
+++ src/components/user/landing/latest/ContentLatest.tsx
@@ -10,6 +10,8 @@ import {
   PaginationPrevious,
   PaginationNext
 } from "@components/ui/pagination";
+import useGetBlogsPublishList from "@hooks/api/get/useGetBlogsPublishListQuery";
+import DOMPurify from "dompurify";
 
 export const ellipsis = (text: string, maxLength: number): string => {
   if (text.length <= maxLength) {
@@ -20,92 +22,63 @@ export const ellipsis = (text: string, maxLength: number): string => {
 };
 
 const ContentLatest: React.FC = () => {
-  const itemsPerPage = 4;
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const paginatedData = blogData
-    .sort(function (a, b) {
-      return ("" + b.createdAt).localeCompare(a.createdAt);
-    })
-    .filter(item => item.category === "News")
-    .slice(startIndex, endIndex);
-
-  const totalPages = Math.ceil(paginatedData.length / itemsPerPage + 1);
-
-  const handlePreviousPage = () => {
-    setCurrentPage(currentPage - 1);
-  };
-
-  const handleNextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
-
+  const { data: blogData } = useGetBlogsPublishList();
+  console.log(blogData);
   return (
     <div className="px-28 pb-8">
       <div className="grid grid-cols-3 lg:grid-cols-2 sm:grid-cols-2 grid-rows-2 gap-5">
-        {paginatedData.map((item, index) => {
-          const correspondingImages = blog_image.filter(
-            image => image.blogId === item.blogId
-          );
-          const thumbnail = correspondingImages.find(image => image.thumbnail);
-          return (
-            <Link to={`/blogs/view/${item.blogId}`} key={index}>
-              <div className="group flex flex-col">
-                <div className="max-h-370px max-w-750px ">
-                  <img
-                    src={thumbnail ? thumbnail.image : "fallback_image_url"}
-                    alt={item.title}
-                    className="w-full rounded-t-lg max-h-64 min-h-64 object-cover"
-                  />
-                </div>
+        {blogData?.data
+          ?.filter(item => item.category === "News")
+          .slice(0, 4)
+          .map((item, index) => {
+            const d = new Date(item?.createdat ?? "");
 
-                <div className="mt-3 pb-3">
-                  <h5 className="text-gray-600 pt-1 text-sm">
-                    {formatDateTime(item.createdAt)}
-                  </h5>
-                  <div className="flex-row grid-flow-row py-3">
-                    <span className="">
-                      {item.tags.map(tags => (
-                        <span className="text-base text-primary rounded-md w-32 border border-[#BBE3AD] bg-secondary px-2 mr-2 py-1 text-center">
-                          {tags}
-                        </span>
-                      ))}
-                    </span>
+            const createDate = d.toLocaleString("en-US", {
+              month: "long",
+              day: "numeric",
+              year: "numeric"
+            });
+
+            const htmlContent = DOMPurify.sanitize(item?.content ?? "");
+            return (
+              <Link to={`/blogs/view/${item.id}`} key={index}>
+                <div className="group flex flex-col">
+                  <div className="max-h-370px max-w-750px ">
+                    <img
+                      src={item?.thumbnail}
+                      alt={item.title}
+                      className="w-full rounded-t-lg max-h-64 min-h-64 object-cover"
+                    />
                   </div>
-                  <h5 className="font-bold mt-1">
-                    {item.category}{" "}
-                    <span className="text-green-700">{">"}</span>
-                  </h5>
-                  <h1 className="text-gray-800 duration-150 group-hover:text-green-700 font-semibold text-lg">
-                    {ellipsis(item.title, 30)}
-                  </h1>
-                  <p className="text-sm me-8 text-justify">
-                    {ellipsis(item.content, 180)}
-                  </p>
+
+                  <div className="mt-3 pb-3">
+                    <h5 className="text-gray-600 pt-1 text-sm">{createDate}</h5>
+                    <div className="flex-row grid-flow-row py-3">
+                      <span className="">
+                        {item?.tags?.map(tags => (
+                          <span className="text-base text-primary rounded-md w-32 border border-[#BBE3AD] bg-secondary px-2 mr-2 py-1 text-center">
+                            {tags.tag}
+                          </span>
+                        ))}
+                      </span>
+                    </div>
+                    <h5 className="font-bold mt-1">
+                      {item.category}{" "}
+                      <span className="text-green-700">{">"}</span>
+                    </h5>
+                    <h1 className="text-gray-800 duration-150 group-hover:text-green-700 font-semibold text-lg line-clamp-2">
+                      {item?.title}
+                    </h1>
+                    <p
+                      className="text-sm me-8 text-justify line-clamp-3"
+                      dangerouslySetInnerHTML={{ __html: htmlContent }}
+                    ></p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          );
-        })}
+              </Link>
+            );
+          })}
       </div>
-      <Pagination>
-        <PaginationContent>
-          <PaginationPrevious onClick={handlePreviousPage} />
-          {[...Array(totalPages)].map((_, index) => (
-            <PaginationItem key={index}>
-              <PaginationLink
-                isActive={index + 1 === currentPage}
-                onClick={() => setCurrentPage(index + 1)}
-              >
-                {index + 1}
-              </PaginationLink>
-            </PaginationItem>
-          ))}
-          <PaginationNext onClick={handleNextPage} className="my-8" />
-        </PaginationContent>
-      </Pagination>
     </div>
   );
 };

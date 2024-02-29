383ff105ba3394c7d28c4c661d9a41075ec4cf62
anotherinthefire <rongodfreyultra@gmail.com>
2024-02-29 20:45:59 +0800

    kwakwakwakwak

--- src/components/admin/events/table/table-events-archive/columns.tsx
+++ src/components/admin/events/table/table-events-archive/columns.tsx
@@ -1,19 +1,9 @@
 import * as React from "react";
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
 
 import { Button } from "@components/ui/button";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from "@components/ui/dropdown-menu";
-
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { EventDetails } from "../../../../../api/openapi";
 import Loader from "../../../../../icons/Loader";
 import usePutEventsUnarchieve from "../../../../../hooks/api/put/usePutEventsUnarchieve";

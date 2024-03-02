c43313e09419c17374cdfff0c7ad60db823b5d44
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-03-03 04:25:24 +0800

    feat: forum overview

--- src/pages/admin/forums/forums.tsx
+++ src/pages/admin/forums/forums.tsx
@@ -9,10 +9,13 @@ import {
   CardDescription
 } from "@components/ui/card";
 import withAuthGuard from "@higher-order/account/withAuthGuard";
+import useGetReportForumsCount from "../../../hooks/api/get/useGetReportForumsCount";
 
 const breadcrumbItems = [{ title: "Forum Management", link: "/admin/forum" }];
 
 const Forums = () => {
+  const { data: forumsCount } = useGetReportForumsCount();
+  console.log(forumsCount);
   return (
     <AdminOutletContainer>
       <BreadCrumb items={breadcrumbItems} />
@@ -20,25 +23,25 @@ const Forums = () => {
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         <Card className="flex justify-start items-center">
           <CardHeader>
-            <CardTitle>10390</CardTitle>
+            <CardTitle>{forumsCount?.forums}</CardTitle>
             <CardDescription>Total Questions in Forums</CardDescription>
           </CardHeader>
         </Card>
         <Card className="flex justify-start items-center">
           <CardHeader>
-            <CardTitle>127242</CardTitle>
+            <CardTitle>{forumsCount?.forums_answers}</CardTitle>
             <CardDescription>Total Answers in Forums</CardDescription>
           </CardHeader>
         </Card>
         <Card className="flex justify-start items-center">
           <CardHeader>
-            <CardTitle>1840</CardTitle>
+            <CardTitle>{forumsCount?.forums_tags}</CardTitle>
             <CardDescription>
               Total Tags Defined in Questions and Answers
             </CardDescription>
           </CardHeader>
         </Card>
-        <Card className="col-span-full md:col-span-2 lg:col-span-3 p-5">
+        <Card className="col-span-full md:col-span-2 lg:col-span-3 p-5 pb-20">
           <CardHeader>
             Comparison of total question and answer each month
           </CardHeader>

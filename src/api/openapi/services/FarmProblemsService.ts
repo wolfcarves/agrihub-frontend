c7db0d1fac7a0ddd9ff407fa01efb99295ae796f
Danielzxccc <103251753+Danielzxccc@users.noreply.github.com>
2024-03-05 06:13:45 +0800

    Daniel dev (#240)
    * fix: access control sidebar
    
    * disable service worker
    
    * fix notification
    
    * fix: socket provider
    
    * test socket provider
    
    * fix: socket controller
    
    * feat: farm problem list
    
    * iniital create farm problem
    
    * feat: problem creation
    
    * feat: update farm problem

--- src/api/openapi/services/FarmProblemsService.ts
+++ src/api/openapi/services/FarmProblemsService.ts
@@ -2,350 +2,348 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { CommunityFarmProblemListResponse } from '../models/CommunityFarmProblemListResponse';
-import type { FarmProblemListResponse } from '../models/FarmProblemListResponse';
-import type { FarmProblemPostResponse } from '../models/FarmProblemPostResponse';
-import type { FarmProblemResponse } from '../models/FarmProblemResponse';
-import type { MarkProblemAsResolvedRequest } from '../models/MarkProblemAsResolvedRequest';
-import type { MessageResponse } from '../models/MessageResponse';
-import type { NewFarmProblem } from '../models/NewFarmProblem';
-import type { ReportRequestBody } from '../models/ReportRequestBody';
-import type { ReportResponse } from '../models/ReportResponse';
+import type { CommunityFarmProblemListResponse } from "../models/CommunityFarmProblemListResponse";
+import type { FarmProblemListResponse } from "../models/FarmProblemListResponse";
+import type { FarmProblemPostResponse } from "../models/FarmProblemPostResponse";
+import type { FarmProblemResponse } from "../models/FarmProblemResponse";
+import type { MarkProblemAsResolvedRequest } from "../models/MarkProblemAsResolvedRequest";
+import type { MessageResponse } from "../models/MessageResponse";
+import type { NewFarmProblem } from "../models/NewFarmProblem";
+import type { ReportRequestBody } from "../models/ReportRequestBody";
+import type { ReportResponse } from "../models/ReportResponse";
 
-import type { CancelablePromise } from '../core/CancelablePromise';
-import { OpenAPI } from '../core/OpenAPI';
-import { request as __request } from '../core/request';
+import type { CancelablePromise } from "../core/CancelablePromise";
+import { OpenAPI } from "../core/OpenAPI";
+import { request as __request } from "../core/request";
 
 export class FarmProblemsService {
-
+  /**
+   * Retrieve farm problem details by ID
+   * @returns FarmProblemResponse Successful response
+   * @throws ApiError
+   */
+  public static getApiFarmProblems({
+    id
+  }: {
     /**
-     * Retrieve farm problem details by ID
-     * @returns FarmProblemResponse Successful response
-     * @throws ApiError
+     * ID of the farm problem to retrieve
      */
-    public static getApiFarmProblems({
-id,
-}: {
-/**
- * ID of the farm problem to retrieve
- */
-id: string,
-}): CancelablePromise<FarmProblemResponse> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/farm/problems/{id}',
-            path: {
-                'id': id,
-            },
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
+    id: string;
+  }): CancelablePromise<FarmProblemResponse> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/farm/problems/{id}",
+      path: {
+        id: id
+      },
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
 
+  /**
+   * Create a new farm problem
+   * @returns FarmProblemPostResponse Successful response
+   * @throws ApiError
+   */
+  public static postApiFarmProblems({
+    requestBody
+  }: {
+    requestBody: NewFarmProblem;
+  }): CancelablePromise<FarmProblemPostResponse> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/farm/problems",
+      body: requestBody,
+      mediaType: "application/json"
+    });
+  }
+
+  /**
+   * Archive a problems
+   * @returns any Problem archived successfully
+   * @throws ApiError
+   */
+  public static deleteApiFarmProblemsArchive({
+    id
+  }: {
     /**
-     * Create a new farm problem
-     * @returns FarmProblemPostResponse Successful response
-     * @throws ApiError
+     * The ID of the problem to be archived
      */
-    public static postApiFarmProblems({
-requestBody,
-}: {
-requestBody: NewFarmProblem,
-}): CancelablePromise<FarmProblemPostResponse> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/farm/problems',
-            body: requestBody,
-            mediaType: 'application/json',
-        });
-    }
+    id: string;
+  }): CancelablePromise<{
+    message?: string;
+  }> {
+    return __request(OpenAPI, {
+      method: "DELETE",
+      url: "/api/farm/problems/archive/{id}",
+      path: {
+        id: id
+      },
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
 
+  /**
+   * Unarchive a problems
+   * @returns any Event Problem successfully
+   * @throws ApiError
+   */
+  public static putApiFarmProblemsUnarchive({
+    id
+  }: {
     /**
-     * Archive a problems
-     * @returns any Problem archived successfully
-     * @throws ApiError
+     * The ID of the problem to be unarchived
      */
-    public static deleteApiFarmProblemsArchive({
-id,
-}: {
-/**
- * The ID of the problem to be archived
- */
-id: string,
-}): CancelablePromise<{
-message?: string;
-}> {
-        return __request(OpenAPI, {
-            method: 'DELETE',
-            url: '/api/farm/problems/archive/{id}',
-            path: {
-                'id': id,
-            },
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
+    id: string;
+  }): CancelablePromise<{
+    message?: string;
+  }> {
+    return __request(OpenAPI, {
+      method: "PUT",
+      url: "/api/farm/problems/unarchive/{id}",
+      path: {
+        id: id
+      },
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
 
+  /**
+   * Delete a problem material
+   * @returns any Event Problem successfully
+   * @throws ApiError
+   */
+  public static deleteApiFarmProblemsMaterial({
+    id
+  }: {
     /**
-     * Unarchive a problems
-     * @returns any Event Problem successfully
-     * @throws ApiError
+     * The ID of the material to be deleted
      */
-    public static putApiFarmProblemsUnarchive({
-id,
-}: {
-/**
- * The ID of the problem to be unarchived
- */
-id: string,
-}): CancelablePromise<{
-message?: string;
-}> {
-        return __request(OpenAPI, {
-            method: 'PUT',
-            url: '/api/farm/problems/unarchive/{id}',
-            path: {
-                'id': id,
-            },
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
+    id: string;
+  }): CancelablePromise<{
+    message?: string;
+  }> {
+    return __request(OpenAPI, {
+      method: "DELETE",
+      url: "/api/farm/problems/material/{id}",
+      path: {
+        id: id
+      },
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
 
+  /**
+   * Get a list of farm problems
+   * @returns FarmProblemListResponse Successful response
+   * @throws ApiError
+   */
+  public static getApiFarmProblemsList({
+    search,
+    page,
+    perpage,
+    filter
+  }: {
     /**
-     * Delete a problem material
-     * @returns any Event Problem successfully
-     * @throws ApiError
+     * Filter problems by search keyword
      */
-    public static deleteApiFarmProblemsMaterial({
-id,
-}: {
-/**
- * The ID of the material to be deleted
- */
-id: string,
-}): CancelablePromise<{
-message?: string;
-}> {
-        return __request(OpenAPI, {
-            method: 'DELETE',
-            url: '/api/farm/problems/material/{id}',
-            path: {
-                'id': id,
-            },
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
-
+    search?: string;
     /**
-     * Get a list of farm problems
-     * @returns FarmProblemListResponse Successful response
-     * @throws ApiError
+     * Page number for pagination
      */
-    public static getApiFarmProblemsList({
-search,
-page,
-perpage,
-filter,
-}: {
-/**
- * Filter problems by search keyword
- */
-search?: string,
-/**
- * Page number for pagination
- */
-page?: string,
-/**
- * Number of items per page
- */
-perpage?: string,
-/**
- * Apply additional filters
- */
-filter?: string,
-}): CancelablePromise<FarmProblemListResponse> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/farm/problems/list',
-            query: {
-                'search': search,
-                'page': page,
-                'perpage': perpage,
-                'filter': filter,
-            },
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
-
+    page?: string;
     /**
-     * Get a list of farm problems
-     * @returns FarmProblemListResponse Successful response
-     * @throws ApiError
+     * Number of items per page
      */
-    public static getApiFarmProblemsArchivedList({
-search,
-page,
-perpage,
-filter,
-}: {
-/**
- * Filter problems by search keyword
- */
-search?: string,
-/**
- * Page number for pagination
- */
-page?: string,
-/**
- * Number of items per page
- */
-perpage?: string,
-/**
- * Apply additional filters
- */
-filter?: string,
-}): CancelablePromise<FarmProblemListResponse> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/farm/problems/archived/list',
-            query: {
-                'search': search,
-                'page': page,
-                'perpage': perpage,
-                'filter': filter,
-            },
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
-
+    perpage?: string;
     /**
-     * Report a farm problem
-     * @returns ReportResponse Successful response
-     * @throws ApiError
+     * Apply additional filters
      */
-    public static postApiFarmProblemsReport({
-requestBody,
-}: {
-requestBody: ReportRequestBody,
-}): CancelablePromise<ReportResponse> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/farm/problems/report',
-            body: requestBody,
-            mediaType: 'application/json',
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
+    filter?: string;
+  }): CancelablePromise<FarmProblemListResponse> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/farm/problems/list",
+      query: {
+        search: search,
+        page: page,
+        perpage: perpage,
+        filter: filter
+      },
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
 
+  /**
+   * Get a list of farm problems
+   * @returns FarmProblemListResponse Successful response
+   * @throws ApiError
+   */
+  public static getApiFarmProblemsArchivedList({
+    search,
+    page,
+    perpage,
+    filter
+  }: {
     /**
-     * List community farm problems
-     * @returns CommunityFarmProblemListResponse Successful response
-     * @throws ApiError
+     * Filter problems by search keyword
      */
-    public static getApiFarmProblemsCommunityList({
-search,
-page,
-perpage,
-filter,
-}: {
-/**
- * Search term to filter problems
- */
-search?: string,
-/**
- * Page number for pagination
- */
-page?: string,
-/**
- * Number of items per page
- */
-perpage?: string,
-/**
- * Filter by problem status
- */
-filter?: 'pending' | 'resolved',
-}): CancelablePromise<CommunityFarmProblemListResponse> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/farm/problems/community/list',
-            query: {
-                'search': search,
-                'page': page,
-                'perpage': perpage,
-                'filter': filter,
-            },
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
-
+    search?: string;
     /**
-     * Mark community farm problem as resolved
-     * @returns MessageResponse Successful response
-     * @throws ApiError
+     * Page number for pagination
      */
-    public static postApiFarmProblemsCommunityResolve({
-id,
-requestBody,
-}: {
-/**
- * The ID of the problem to mark as resolved
- */
-id: string,
-requestBody: MarkProblemAsResolvedRequest,
-}): CancelablePromise<MessageResponse> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/farm/problems/community/resolve/{id}',
-            path: {
-                'id': id,
-            },
-            body: requestBody,
-            mediaType: 'application/json',
-            errors: {
-                400: `Validation Error`,
-                401: `Unauthorized`,
-                404: `Not Found Error`,
-                500: `Server Error`,
-            },
-        });
-    }
+    page?: string;
+    /**
+     * Number of items per page
+     */
+    perpage?: string;
+    /**
+     * Apply additional filters
+     */
+    filter?: string;
+  }): CancelablePromise<FarmProblemListResponse> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/farm/problems/archived/list",
+      query: {
+        search: search,
+        page: page,
+        perpage: perpage,
+        filter: filter
+      },
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
 
+  /**
+   * Report a farm problem
+   * @returns ReportResponse Successful response
+   * @throws ApiError
+   */
+  public static postApiFarmProblemsReport({
+    requestBody
+  }: {
+    requestBody: ReportRequestBody;
+  }): CancelablePromise<ReportResponse> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/farm/problems/report",
+      body: requestBody,
+      mediaType: "application/json",
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
+
+  /**
+   * List community farm problems
+   * @returns CommunityFarmProblemListResponse Successful response
+   * @throws ApiError
+   */
+  public static getApiFarmProblemsCommunityList({
+    search,
+    page,
+    perpage,
+    filter
+  }: {
+    /**
+     * Search term to filter problems
+     */
+    search?: string;
+    /**
+     * Page number for pagination
+     */
+    page?: string;
+    /**
+     * Number of items per page
+     */
+    perpage?: string;
+    /**
+     * Filter by problem status
+     */
+    filter?: "pending" | "resolved";
+  }): CancelablePromise<CommunityFarmProblemListResponse> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/farm/problems/community/list",
+      query: {
+        search: search,
+        page: page,
+        perpage: perpage,
+        filter: filter
+      },
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
+
+  /**
+   * Mark community farm problem as resolved
+   * @returns MessageResponse Successful response
+   * @throws ApiError
+   */
+  public static postApiFarmProblemsCommunityResolve({
+    id,
+    requestBody
+  }: {
+    /**
+     * The ID of the problem to mark as resolved
+     */
+    id: string;
+    requestBody: MarkProblemAsResolvedRequest;
+  }): CancelablePromise<MessageResponse> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/farm/problems/community/resolve/{id}",
+      path: {
+        id: id
+      },
+      body: requestBody,
+      mediaType: "application/json",
+      errors: {
+        400: `Validation Error`,
+        401: `Unauthorized`,
+        404: `Not Found Error`,
+        500: `Server Error`
+      }
+    });
+  }
 }
---
+++ src/components/admin/farms/learning-material.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+import {
+  FieldArrayWithId,
+  UseFieldArrayAppend,
+  UseFieldArrayRemove
+} from "react-hook-form";
+import { convertToEmbedLink, formatDate } from "../../lib/utils";
+import { FaRegTrashCan } from "react-icons/fa6";
+import parse from "html-react-parser";
+import { Material, Problem } from "./form/farm-problem-schema";
+import { useMutation } from "@tanstack/react-query";
+import { FarmProblemsService } from "../../../api/openapi";
+import { toast } from "sonner";
+import LoadingSpinner from "../../../icons/LoadingSpinner";
+
+type LearningMaterialProps = {
+  items: Material;
+  index: number;
+  setSelection: React.Dispatch<React.SetStateAction<boolean>>;
+  fields: FieldArrayWithId<Problem, "materials", "id">[];
+  problemId: string | undefined;
+  append?: UseFieldArrayAppend<Problem, "materials">;
+  remove?: UseFieldArrayRemove;
+};
+export default function LearningMaterial({
+  items,
+  index,
+  fields,
+  setSelection,
+  append,
+  problemId,
+  remove
+}: LearningMaterialProps) {
+  const { mutateAsync, isLoading: IsMutationLoading } = useMutation(
+    ["REMOVE_LEARNING_MATERIAL_KEY"],
+    {
+      async mutationFn(id: string) {
+        const response =
+          await FarmProblemsService.deleteApiFarmProblemsMaterial({
+            id
+          });
+        return response;
+      },
+      onSuccess: () => {}
+    }
+  );
+  return (
+    <article
+      className="max-w-sm mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm"
+      onClick={() => {
+        // e.stopPropagation();
+        const isInList = fields.some(learn => learn.title === items.title);
+        if (append && !isInList) {
+          append(items);
+        }
+        setSelection(false);
+      }}
+    >
+      <div className="cursor-pointer">
+        <div className="relative">
+          <div className="h-48 rounded-t-md">
+            {items.thumbnail.type === "image" ? (
+              <img
+                src={`https://s3.ap-southeast-1.amazonaws.com/agrihub-bucket/${items.thumbnail.resource}`}
+                alt={items.thumbnail.id}
+                className="w-full aspect-video object-cover object-center rounded-md h-48 rounded-t-md"
+              />
+            ) : items.thumbnail.type === "video" ? (
+              <div className="w-full aspect-video h-48 rounded-t-md">
+                <iframe
+                  className="w-full h-full rounded-t-md"
+                  src={convertToEmbedLink(items.thumbnail.resource || "")}
+                  title={items.thumbnail.id}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            ) : null}
+          </div>
+          {remove && (
+            <button
+              type="button"
+              className="absolute top-0 right-0 text-white bg-red-600 rounded-full p-1 cursor-pointer"
+              onClick={async () => {
+                if (remove) {
+                  if (problemId && items.fpm_id) {
+                    try {
+                      await mutateAsync(items.fpm_id ?? "");
+                    } catch (error: any) {
+                      toast.error(error.body.message);
+                    }
+                  }
+
+                  remove(index);
+                }
+              }}
+            >
+              {IsMutationLoading ? (
+                <LoadingSpinner className="absolute" />
+              ) : (
+                <FaRegTrashCan />
+              )}
+            </button>
+          )}
+        </div>
+
+        <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
+          <div className="">
+            <span className="block text-gray-400 text-sm">
+              {formatDate(items.createdat)}
+            </span>
+          </div>
+        </div>
+        <div className="pt-3 ml-4 mr-2 mb-3 ">
+          <h3 className="text-xl text-gray-900 truncate">{items.title}</h3>
+          <p className="text-gray-400 text-sm mt-1 line-clamp-3">
+            {parse(items.content || "")}
+          </p>
+
+          <div className="my-4 item">
+            <p className="text-gray-700 mb-2 flex flex-wrap">
+              {items.tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="text-base text-primary rounded-md w-auto border border-[#BBE3AD] bg-secondary px-2 mr-2 mb-2 py-1"
+                >
+                  {tag.tag}
+                </span>
+              ))}
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}

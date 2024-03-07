fac7441a01bb266f2c0bf22d8bdb2f9bc9985246
anotherinthefire <rongodfreyultra@gmail.com>
2024-03-08 07:52:48 +0800

    style: login back button

--- src/pages/user/account/login.tsx
+++ src/pages/user/account/login.tsx
@@ -2,6 +2,8 @@ import UserLoginForm from "@components/user/account/forms/UserLoginForm/UserLogi
 import UserLoginFormTitle from "@components/user/account/title/UserLoginFormTitle";
 import withAuthGuard from "@higher-order/account/withAuthGuard";
 import { Helmet } from "react-helmet-async";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -9,6 +11,13 @@ const Login = () => {
       <Helmet>
         <title>Login to Agrihub</title>
       </Helmet>
+      <div className="mb-10 w-max">
+        <Link to="/">
+          <span className="flex items-center gap-x-2 text-foreground font-poppins-semibold hover:underline hover:underline-offset-2 py-2.5 px-1.5 rounded-lg duration-200">
+            <FaArrowLeftLong /> Back
+          </span>
+        </Link>
+      </div>
       <UserLoginFormTitle />
       <UserLoginForm />
     </>

4cd040be95c6a40f1f1e69f09accf2bac53bf068
anotherinthefire <rongodfreyultra@gmail.com>
2024-03-09 22:03:34 +0800

    style&refactor: user events module

--- src/pages/user/events/event.tsx
+++ src/pages/user/events/event.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import EventDetailsList from "@components/user/event/list/EventDetailsList";
 import EventTitle from "@components/user/event/title/EventTitle";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Event = () => {
   return (
     <div className="container">
+      <div className="mt-4 sm:mx-8">
+        <Link to="/events">
+          <span className="flex items-center gap-x-2 text-foreground font-poppins-semibold hover:underline hover:underline-offset-2 py-2.5 px-1.5 rounded-lg duration-200">
+            <FaArrowLeftLong /> Back
+          </span>
+        </Link>
+      </div>
       <EventTitle />
       <EventDetailsList />
     </div>

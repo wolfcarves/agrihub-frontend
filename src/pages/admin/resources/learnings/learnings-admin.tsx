f918546d91eb2e33d6afd8110d65bb54a5507de0
anotherinthefire <rongodfreyultra@gmail.com>
2024-03-10 12:26:08 +0800

    refactor: admin resource

--- src/pages/admin/resources/learnings/learnings-admin.tsx
+++ src/pages/admin/resources/learnings/learnings-admin.tsx
@@ -5,12 +5,17 @@ import BreadCrumb from "../../../../components/ui/custom/breadcrumb/breadcrumb";
 import withAuthGuard from "@higher-order/account/withAuthGuard";
 import DialogAddLearning from "../../../../components/admin/learning-materials/dialogs/dialog-add-learning/dialog-add-learning";
 import TableLearningMaterial from "../../../../components/admin/learning-materials/tables/table-learning-material/table-learning-material";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
+import TableLearningMaterialDraft from "@components/admin/learning-materials/tables/table-learning-material-draft/table-learning-material-draft";
+import TableLearningMaterialArchive from "@components/admin/learning-materials/tables/table-learning-material-archive/table-learning-material-archive";
+import { useParams } from "react-router-dom";
 
 const breadcrumbItems = [
   { title: "Resource Management", link: "/admin/resources" },
   { title: "Learning Materials", link: "/admin/resource/learnings" }
 ];
 const LearningsAdmin = () => {
+  const { tab }: any = useParams();
   return (
     <AdminOutletContainer className="container mx-auto py-10 ">
       <BreadCrumb items={breadcrumbItems} />
@@ -24,7 +29,25 @@ const LearningsAdmin = () => {
         Manage all learning materials.
       </p>
       <hr className="my-4" />
-      <TableLearningMaterial />
+      <Tabs defaultValue={tab || "published"}>
+        <TabsList>
+          <TabsTrigger value="published">Published</TabsTrigger>
+          <TabsTrigger value="draft">Drafts</TabsTrigger>
+          <TabsTrigger value="archived">Archived</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="published">
+          <TableLearningMaterial />
+        </TabsContent>
+
+        <TabsContent value="draft">
+          <TableLearningMaterialDraft />
+        </TabsContent>
+
+        <TabsContent value="archived">
+          <TableLearningMaterialArchive />
+        </TabsContent>
+      </Tabs>
     </AdminOutletContainer>
   );
 };

79b40c3ebf68e7cf75bf9569679cc99a50c27b86
anotherinthefire <rongodfreyultra@gmail.com>
2024-03-10 12:18:52 +0800

    refactor&style: admin web sidebar

--- src/components/admin/layout/sidebar/admin-sidebar.tsx
+++ src/components/admin/layout/sidebar/admin-sidebar.tsx
@@ -1,74 +1,49 @@
 import React, { useState } from "react";
 import logo from "../../../../icons/main-logo.svg";
 import { adminNavigation } from "./admin-menu";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../../../../globals.css";
 import AdminSidebarNavLink from "./admin-sidebar-navlink";
 import useGetMyProfileQuery from "@hooks/api/get/useGetMyProfileQuery";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut
+} from "@components/ui/command";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const AdminSidebar = () => {
-  const [clickedItem, setClickedItem] = useState("Overview");
+  const [clickedItem, setClickedItem] = useState<string | null>(null);
   const [open, setOpen] = useState(true);
+  const navigate = useNavigate();
+  const [commandInputValue, setCommandInputValue] = useState("");
 
   const { data: authData, isLoading: isAuthDataLoading } =
     useGetMyProfileQuery();
 
-  const handleClick = (itemName: string) => {
-    if (clickedItem === itemName) {
+  const handleParentClick = (itemName: string) => {
+    setClickedItem(prevClickedItem =>
+      prevClickedItem === itemName ? null : itemName
+    ); // Toggle clickedItem state
+  };
+
+  const handleClick = () => {
+    if (open) {
       setOpen(!open);
     } else {
-      setClickedItem(itemName);
       setOpen(true);
     }
   };
 
   return (
     <>
-      <nav className="top-0 left-0 w-20 h-full border-r bg-white space-y-8 hidden sm:block">
-        <div className="flex flex-col h-full">
-          <div className="h-20 flex items-center justify-center px-8">
-            <Link to="/admin/dashboard" className="flex-none">
-              <img
-                src={logo as unknown as string}
-                width={35}
-                className="mx-auto"
-              />
-            </Link>
-          </div>
-          <div className="flex-1 flex flex-col h-full">
-            <ul className="px-4 text-sm font-medium flex-1">
-              {adminNavigation.map((item, idx) => {
-                const checkAuthorization = authData?.[item.module];
-                // let checkSubModule: boolean
-
-                // if (item.name === "Resources Management"){
-                //   item.nav.
-                // }
-                if (!checkAuthorization) {
-                  return <></>;
-                }
-
-                return (
-                  <div key={idx}>
-                    <button
-                      onClick={() => handleClick(item.name)}
-                      className={`relative flex items-center justify-center gap-x-2 text-gray-600 p-2 rounded-lg  hover:bg-green-50 active:bg-green-100 duration-150 group ${
-                        clickedItem === item.name ? "bg-gray-200" : ""
-                      }`}
-                    >
-                      <div className="text-gray-500">{item.icon}</div>
-                      <span className="absolute left-14 p-1 px-1.5 rounded-md whitespace-nowrap text-xs text-white bg-gray-800 hidden group-hover:inline-block group-focus:hidden duration-150">
-                        {item.name}
-                      </span>
-                    </button>
-                  </div>
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-      </nav>
-
       <div
         className={`top-0 left-0 h-full border-r bg-white space-y-8 overflow-y-auto overflow-x-hidden no-scrollbar sm:relative ml-[4.5rem] sm:ml-0 absolute z-40 ${
           open ? "sm:block hidden w-80" : "sm:hidden block w-0"
@@ -76,38 +51,86 @@ const AdminSidebar = () => {
       >
         <div className={`${open ? "block w-64" : "hidden w-0"} my-10`}>
           <h4 className="pb-3 px-4 font-bold text-gray-800 md:px-8 ">
-            {clickedItem && `${clickedItem}`}
+            <div className="flex items-center justify-center px-8">
+              <div className="flex-none">
+                <img
+                  src={logo as unknown as string}
+                  width={50}
+                  className="mx-auto"
+                  onClick={() => handleClick()}
+                />
+              </div>
+            </div>
           </h4>
-          <div className="text-gray-600 px-4 md:px-8">
-            <ul>
-              {adminNavigation
-                .filter(item => item.name === clickedItem)
-                .map((item, idx) =>
-                  item.nav.map((navItem, navIdx) => {
-                    const checkAuthorization = authData?.[navItem.module];
 
-                    if (!checkAuthorization) {
-                      return <></>;
-                    }
+          <Command>
+            <CommandInput placeholder="Search.module.." />
 
-                    return (
+            <CommandList>
+              {adminNavigation.map((item, idx) => {
+                const checkAuthorization = authData?.[item.module];
+                if (!checkAuthorization) {
+                  return null;
+                }
+                return (
+                  <React.Fragment key={idx}>
+                    <div className="flex gap-4 items-center">
+                      <AdminSidebarNavLink
+                        to={item.href}
+                        logo={item.icon}
+                        end={true}
+                        title={item.name}
+                        onClick={() => {
+                          if (item.nav.length > 1) {
+                            handleParentClick(item.name);
+                          } else {
+                            navigate(item.nav[0].href);
+                          }
+                        }}
+                      />
+                    </div>
+                    {clickedItem === item.name && item.nav.length > 1 && (
                       <>
-                        <h5 className="px-4 font-bold text-gray-800">
-                          {navItem.title}
-                        </h5>
-                        <AdminSidebarNavLink
-                          to={navItem.href}
-                          key={`${idx}-${navIdx}`}
-                          logo
-                          end={true}
-                          title={navItem.name}
-                        ></AdminSidebarNavLink>
+                        {item.nav.map((navItem, navIdx) => {
+                          const subCheckAuthorization =
+                            authData?.[navItem.module];
+                          if (!subCheckAuthorization) {
+                            return null;
+                          }
+                          return (
+                            <CommandGroup
+                              heading={navItem.title}
+                              key={`${idx}-${navIdx}`}
+                              className="bg-green-100"
+                            >
+                              <CommandItem>
+                                <AdminSidebarNavLink
+                                  to={navItem.href}
+                                  logo
+                                  end={true}
+                                  title={navItem.name}
+                                />
+                              </CommandItem>
+                            </CommandGroup>
+                          );
+                        })}
                       </>
-                    );
-                  })
-                )}
-            </ul>
-          </div>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            </CommandList>
+          </Command>
+        </div>
+      </div>
+      <div
+        className={`cursor-pointer hidden items-center justify-center p-5 sm:block absolute z-50 ${
+          open ? "top-0 left-64 " : "top-0 left-0"
+        }`}
+        onClick={() => handleClick()}
+      >
+        <div className="flex-none">
+          <RxHamburgerMenu size={24} />
         </div>
       </div>
     </>

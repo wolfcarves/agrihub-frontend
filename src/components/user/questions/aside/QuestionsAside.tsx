aca16e60fcdc421b7f25deca65cddad491b446c2
Rodel <rodel.crisosto7@gmail.com>
2024-03-28 11:02:57 +0800

    chore: tag links

--- src/components/user/questions/aside/QuestionsAside.tsx
+++ src/components/user/questions/aside/QuestionsAside.tsx
@@ -20,28 +20,12 @@ const QuestionsAside = () => {
           <LoadingSpinner className="mx-auto text-md text-green-700" />
         ) : (
           tagsData?.tags?.map(({ id, tag_name }) => (
-            <Link to="/" key={id}>
+            <Link to={`/forum?tag=${tag_name}`} key={id}>
               <UserAsideItem>{tag_name}</UserAsideItem>
             </Link>
           ))
         )}
       </UserAsideItemContent>
-
-      {/* <UserAsideTitle className="mt-5">Discover Tags</UserAsideTitle>
-
-      <UserAsideItemContent className="mt-5 flex flex-wrap gap-x-1 gap-y-2 w-[16rem]">
-        {isTagsLoading ? (
-          <LoadingSpinner className="mx-auto text-md text-green-700" />
-        ) : (
-          tagsData?.tags?.map(({ id, tag_name }) => {
-            return (
-              <Link to={`/forum/tag/${id}`} key={id}>
-                <TagChip name={tag_name} size="sm" />
-              </Link>
-            );
-          })
-        )}
-      </UserAsideItemContent> */}
     </UserAside>
   );
 };

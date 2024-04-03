602ec2ddd8478629915fd5fd7a7af7dd7bd5ab51
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-04-03 17:04:26 +0800

    chore: update codegen

--- src/api/openapi/models/NewCommunityCropReport.ts
+++ src/api/openapi/models/NewCommunityCropReport.ts
@@ -4,17 +4,17 @@
 /* eslint-disable */
 
 export type NewCommunityCropReport = {
-  crop_id?: string;
-  planted_qty?: number;
-  is_other?: boolean;
-  isyield?: boolean;
-  c_name?: string;
-  harvested_qty?: number;
-  withered_crops?: number;
-  date_planted?: string;
-  date_harvested?: string;
-  notes?: string;
-  is_first_report?: string;
-  kilogram?: string;
-  image?: Array<Blob>;
+    crop_id?: string;
+    planted_qty?: number;
+    is_other?: boolean;
+    isyield?: boolean;
+    c_name?: string;
+    harvested_qty?: number;
+    withered_crops?: number;
+    date_planted?: string;
+    date_harvested?: string;
+    notes?: string;
+    is_first_report?: string;
+    kilogram?: string;
+    image?: Array<Blob>;
 };

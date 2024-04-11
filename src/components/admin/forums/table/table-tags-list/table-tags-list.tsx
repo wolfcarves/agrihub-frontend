e674bcf292c124e3c448246e8155032a08cbd240
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-04-11 19:18:38 +0800

    feat: other in tool request

--- src/components/admin/forums/table/table-tags-list/table-tags-list.tsx
+++ src/components/admin/forums/table/table-tags-list/table-tags-list.tsx
@@ -24,8 +24,10 @@ const TableTagsList = () => {
   });
   const debouncedSearch = useDebounce((value: string) => {
     searchParams.set("search", value);
+    searchParams.delete("page");
     setSearchParams(searchParams);
   }, 100);
+
   return (
     <>
       <Input

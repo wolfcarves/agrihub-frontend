daca7d8f1cf5a66125dd138416829bbd82e2b633
Rodel Crisosto <89376217+wolfcarves@users.noreply.github.com>
2024-04-15 14:31:55 +0800

    Chore/events pagination (#363)
    * chore : forum labels
    
    * chore : events pagination

--- src/components/ui/custom/button/back-button.tsx
+++ src/components/ui/custom/button/back-button.tsx
@@ -2,18 +2,20 @@ import { ComponentProps } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 
-interface BackButtonProps extends ComponentProps<"button"> {}
+interface BackButtonProps extends ComponentProps<"button"> {
+  title?: string;
+}
 
-const BackButton = ({ className }: BackButtonProps) => {
+const BackButton = ({ className, title }: BackButtonProps) => {
   const navigate = useNavigate();
 
   return (
     <button
       type="button"
-      className={`flex items-center gap-x-2 w-max text-foreground font-poppins-semibold hover:bg-accent py-1 px-3 border rounded-lg duration-200 ${className}`}
+      className={`flex items-center gap-x-2 w-max text-foreground font-poppins-semibold hover:bg-accent p-3 border rounded-xl duration-200 ${className}`}
       onClick={() => navigate(-1)}
     >
-      <FaArrowLeftLong /> Back
+      <FaArrowLeftLong size={17} /> {title}
     </button>
   );
 };

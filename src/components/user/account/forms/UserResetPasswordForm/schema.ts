53cf16ddfdb57dbe3219138860f8e3ca8dec6a16
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-04-27 12:42:50 +0800

    feat: reset password validation and finish profile desc

--- src/components/user/account/forms/UserResetPasswordForm/schema.ts
+++ src/components/user/account/forms/UserResetPasswordForm/schema.ts
@@ -5,9 +5,9 @@ export const resetPasswordSchema = z
     password: z
       .string()
       .min(8, "Password must be at least 8 characters")
-      .max(30, "Password is too much")
-      .regex(new RegExp(".*[A-Z].*"), "Password must have one capital letter")
-      .regex(new RegExp(".*[a-z].*"), "Password must have one capital letter")
+      .max(25, "Password is too much")
+      .regex(new RegExp(".*[A-Z].*"), "Password must have uppercase letter")
+      .regex(new RegExp(".*[a-z].*"), "Password must have lowercase letter")
       .regex(new RegExp(".*[0-9].*"), "Password must have one digit number"),
     confirmPassword: z.string()
   })

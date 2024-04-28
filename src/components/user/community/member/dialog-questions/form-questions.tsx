ab04594868aa26e94ff52dc4760032acce30ca3d
Danielzxccc <103251753+Danielzxccc@users.noreply.github.com>
2024-04-28 21:19:40 +0800

    Daniel dev (#408)
    * fix: access control sidebar
    
    * disable service worker
    
    * fix notification
    
    * fix: socket provider
    
    * test socket provider
    
    * fix: socket controller
    
    * feat: farm problem list
    
    * iniital create farm problem
    
    * feat: problem creation
    
    * feat: update farm problem
    
    * feat: reported problem list in admin
    
    * feat: reported problem list in admin v2
    
    * feat: fix tag upsert issue
    
    * feat: Client CMS v1
    
    * fix: socket provider
    
    * fix vercel build cache
    
    * fix vercel build cache v2
    
    * sw.js directive
    
    * feat: CMS Client Details
    
    * fix vercel.json
    
    * fix: notification badge toggle
    
    * fix sidebar navigation on assistant admin
    
    * fix user notification bell and pop up
    
    * fix: crop details update
    
    * fix: questions CMS community

--- src/components/user/community/member/dialog-questions/form-questions.tsx
+++ src/components/user/community/member/dialog-questions/form-questions.tsx
@@ -1,34 +1,44 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useEffect } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import * as zod from "zod";
 import { Label } from "../../../../ui/label";
 import { Input } from "../../../../ui/input";
 import useCommunityFarmQuestionsCreate from "../../../../../hooks/api/post/useCommunityFarmQuestionsCreate";
 import { toast } from "sonner";
-import { Form, FormMessage } from "../../../../ui/form";
 import Loader from "../../../../../icons/Loader";
 import { useParams } from "react-router-dom";
 import { DialogFooter } from "../../../../ui/dialog";
 import { Button } from "../../../../ui/button";
-import { FarmQuestion } from "../../../../../api/openapi";
+import { FarmQuestion, FarmQuestionItem } from "../../../../../api/openapi";
 import useGetCommunityFarmQuestions from "../../../../../hooks/api/get/useGetCommunityFarmQuestions";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from "@components/ui/form";
+
 interface dialogProps {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
   questionId?: string;
 }
 
 const addQuestionSchema = zod.object({
-  question: zod
-    .string({
-      required_error: "Question is required."
+  questions: zod.array(
+    zod.object({
+      id: zod.string().optional(),
+      farmid: zod.string(),
+      question: zod.string()
     })
-    .min(3, "Question is too small")
+  )
 });
+
 const FormQuestions: React.FC<dialogProps> = ({ setIsOpen, questionId }) => {
   const { id } = useParams();
   const { data: questionData } = useGetCommunityFarmQuestions(id || "");
-  const form = useForm<FarmQuestion>({
+  const form = useForm<zod.infer<typeof addQuestionSchema>>({
     resolver: zodResolver(addQuestionSchema),
     mode: "onBlur"
   });
@@ -36,39 +46,87 @@ const FormQuestions: React.FC<dialogProps> = ({ setIsOpen, questionId }) => {
   const { mutateAsync: formMutate, isLoading: formLoading } =
     useCommunityFarmQuestionsCreate();
 
-  const handleSubmitForm = async (data: FarmQuestion) => {
-    const compiledData: FarmQuestion = {
-      id: questionId ?? null,
-      farmid: id,
-      question: data.map(question => question)
-    };
+  const handleSubmitForm = async (
+    data: zod.infer<typeof addQuestionSchema>
+  ) => {
+    const compiledData = data.questions;
 
+    console.log(compiledData);
     try {
-      await formMutate({ requestBody: data });
+      await formMutate({ requestBody: compiledData });
       toast.success(`Question Created Successfully!`);
       setIsOpen(false);
     } catch (e: any) {
       toast.error(e.body.message);
     }
   };
+
+  const { fields, replace } = useFieldArray({
+    control: form.control,
+    name: "questions",
+    rules: {
+      required: true,
+      minLength: 1,
+      maxLength: 5
+    },
+    keyName: "uid"
+  });
+
+  useEffect(() => {
+    if (questionId) {
+      const questionToEdit = questionData?.find(
+        item => item?.id === questionId
+      );
+
+      replace([questionToEdit] as {
+        farmid: string;
+        question: string;
+        id?: string | undefined;
+      }[]);
+    } else {
+      replace({
+        farmid: id ?? "",
+        question: ""
+      });
+    }
+  }, [questionData]);
+
   return (
     <>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(handleSubmitForm)}
-          encType="multipart/form-data"
           className="grid gap-4"
         >
           <div className="grid gap-4">
-            <div className="flex-col gap-4">
-              <Label className="text-right font-poppins-medium">Question</Label>
-              <Input
-                // {...form.register(`questions`)}
-                placeholder="Insert question"
-                className="col-span-3"
-              />
-              <FormMessage>{/* {form.formState.errors?.} */}</FormMessage>
-            </div>
+            {fields.map((question, index) => (
+              <div className="flex-col gap-4" key={index}>
+                <Label className="text-right font-poppins-medium">
+                  Question
+                </Label>
+                <FormField
+                  control={form.control}
+                  name={`questions.${index}.question`}
+                  defaultValue={question?.question}
+                  // disabled={!isEditing}
+                  render={({ field, fieldState }) => (
+                    <FormItem className="w-full">
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type="text"
+                          placeholder="Insert question"
+                          className="col-span-3"
+                        />
+                      </FormControl>
+                      <FormMessage>{fieldState.error?.message}</FormMessage>
+                    </FormItem>
+                  )}
+                />
+
+                <FormMessage>{/* {form.formState.errors?.} */}</FormMessage>
+              </div>
+            ))}
           </div>
 
           <DialogFooter>

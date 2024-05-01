0f07ae9d6a9c3aa7e4ea9d09d04f766c0401175c
anotherinthefire <rongodfreyultra@gmail.com>
2024-05-02 01:18:31 +0800

    feat: community landing

---
+++ src/icons/community/TaskIllustration.tsx
@@ -0,0 +1,7 @@
+import TaskSrc from "@icons/community/task-illustration.svg";
+
+const TaskIllustration = () => {
+  return <img src={TaskSrc as unknown as string} className="h-64 scale-125" />;
+};
+
+export default TaskIllustration;

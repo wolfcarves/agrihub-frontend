445845f05667d681ce47198eb9d1439d315dfa5a
Danielzxccc <araojodaniel321@gmail.com>
2024-05-07 02:57:42 +0800

    fix socket connection

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 const DEV_STATE = import.meta.env.VITE_DEV_STATE;
-const URL = "https://api.qc-agrihub.xyz";
+const URL = "https://apiv2.qc-agrihub.xyz";
 
 export const socket = io(URL as string);

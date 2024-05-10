aa83ac92707c3f22df715497068f0e57b18260d5
anotherinthefire <rongodfreyultra@gmail.com>
2024-05-10 16:34:43 +0800

    refactor: our blogs filtering our focus

--- src/hooks/api/get/useGetBlogsPublishListQuery.ts
+++ src/hooks/api/get/useGetBlogsPublishListQuery.ts
@@ -6,15 +6,17 @@ export const GET_BLOGS_PUBLISH_LIST = () => "GET_BLOGS_PUBLISH_LIST_KEY";
 export default function useGetBlogsPublishList(
   search?: string,
   page?: string,
-  perpage?: string
+  perpage?: string,
+  filter?: "News" | "Initiatives" | "Our Focus"
 ) {
   return useQuery({
-    queryKey: [GET_BLOGS_PUBLISH_LIST(), search, page, perpage],
+    queryKey: [GET_BLOGS_PUBLISH_LIST(), search, page, perpage, filter],
     queryFn: async () => {
       const response = await BlogsService.getApiBlogsPublishedList({
         search,
         page,
-        perpage
+        perpage,
+        filter
       });
 
       return response;
